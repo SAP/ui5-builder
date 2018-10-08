cc0a11a1504ea09cea080105cd09a008543a3181
Merlin Beutlberger <m.beutlberger@sap.com>
2018-10-08 16:42:16 +0200

    [FIX] Replace copyright and version strings in *.json and .library files
    Currently only placeholders in *.js files are processed
    
    Fixes #76

--- test/expected/build/application.g/dest/Component-preload.js
+++ test/expected/build/application.g/dest/Component-preload.js
@@ -3,5 +3,5 @@ jQuery.sap.registerPreloadedModules({
 "modules":{
 	"application/g/Component.js":function(){sap.ui.define(["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.Component",{metadata:{manifest:"json"}})});
 },
-	"application/g/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.g","type":"application","applicationVersion":{"version":"1.2.2"},"embeds":["embedded"],"title":"{{title}}"}}'
+	"application/g/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.g","type":"application","applicationVersion":{"version":"1.0.0"},"embeds":["embedded"],"title":"{{title}}"},"customCopyrightString":"Some fancy copyright"}'
 }});
