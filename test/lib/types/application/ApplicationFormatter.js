f13682cc3dbba7da72f801d1b843f545021d3a34
Tobias Sorn <tobias.sorn@sap.com>
2018-12-13 13:09:06 +0100

    Improves test coverage for ApplicationFormatter
    Adds sinon as dev dependency to be able to stub/mock/spy in tests

--- test/lib/types/application/ApplicationFormatter.js
+++ test/lib/types/application/ApplicationFormatter.js
@@ -1,6 +1,7 @@
 const {test} = require("ava");
 const path = require("path");
 const chai = require("chai");
+const sinon = require("sinon");
 const assert = require("assert");
 chai.use(require("chai-fs"));
 
@@ -173,11 +174,134 @@ const applicationBTree = {
 	}
 };
 
-test("not existing directory webapp for c3", async (t) => {
+
+test("ApplicationFormatter#validate: not existing directory webapp for c3", async (t) => {
+	const myProject = Object.assign({}, applicationBTree);
 	const applicationFormatter = new ApplicationFormatter();
-	return applicationFormatter.validate(applicationBTree).catch((error) => {
+	return applicationFormatter.validate(myProject).catch((error) => {
 		assert.ok(error.message && error.message.startsWith("Could not find application directory " +
 			"of project application.b: "));
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#validate: project not defined", async (t) => {
+	const applicationFormatter = new ApplicationFormatter();
+	return applicationFormatter.validate(null).catch((error) => {
+		t.is(error.message, "Project is undefined");
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#validate: empty version", async (t) => {
+	const myProject = Object.assign({}, applicationBTree);
+	myProject.version = undefined;
+	const applicationFormatter = new ApplicationFormatter();
+	return applicationFormatter.validate(myProject).catch((error) => {
+		t.is(error.message, "\"version\" is missing for project application.b");
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#validate: empty type", async (t) => {
+	const myProject = Object.assign({}, applicationBTree);
+	myProject.type = undefined;
+	const applicationFormatter = new ApplicationFormatter();
+	return applicationFormatter.validate(myProject).catch((error) => {
+		t.is(error.message, "\"type\" configuration is missing for project application.b");
+		t.pass();
+	});
+});
+
+
+test("ApplicationFormatter#validate: metadata", async (t) => {
+	const myProject = Object.assign({}, applicationBTree);
+	myProject.metadata = undefined;
+	const applicationFormatter = new ApplicationFormatter();
+	return applicationFormatter.validate(myProject).catch((error) => {
+		t.is(error.message, "\"metadata.name\" configuration is missing for project application.b");
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#validate: project resources", async (t) => {
+	const myProject = Object.assign({}, applicationBTree);
+	myProject.resources = undefined;
+	const applicationFormatter = new ApplicationFormatter();
+	return applicationFormatter.validate(myProject).catch((error) => {
+		assert.ok(error.message && error.message.startsWith("Could not find application directory " +
+			"of project application.b: "));
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#readManifest: check json", async (t) => {
+	const myProject = Object.assign({}, applicationBTree);
+	myProject.path = path.join(__dirname, "..", "..", "..", "fixtures", "application.d");
+	const applicationFormatter = new ApplicationFormatter();
+	return applicationFormatter.readManifest(myProject).then((oRes) => {
+		t.is(typeof oRes, "object");
+		t.pass();
+	});
+});
+
+function createMockProject() {
+	return {
+		"resources": {
+			"configuration": {
+				"paths": {
+					"webapp": "webapp"
+				}
+			}
+		},
+		"metadata": {
+			"name": "projectname"
+		}
+	};
+}
+
+test("ApplicationFormatter#format: readManifest fail", async (t) => {
+	const applicationFormatter = new ApplicationFormatter();
+	sinon.stub(applicationFormatter, "validate").resolves();
+	sinon.stub(applicationFormatter, "readManifest").rejects();
+	const project = createMockProject();
+	return applicationFormatter.format(project).then((oRes) => {
+		t.falsy(oRes);
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#format: No 'sap.app' configuration found", async (t) => {
+	const applicationFormatter = new ApplicationFormatter();
+	sinon.stub(applicationFormatter, "validate").resolves();
+	sinon.stub(applicationFormatter, "readManifest").resolves({});
+	const project = createMockProject();
+	return applicationFormatter.format(project).then((oRes) => {
+		t.is(project.resources.pathMappings["/"], "webapp");
+		t.falsy(oRes);
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#format: No application id found", async (t) => {
+	const applicationFormatter = new ApplicationFormatter();
+	sinon.stub(applicationFormatter, "validate").resolves();
+	sinon.stub(applicationFormatter, "readManifest").resolves({"sap.app": {}});
+	const project = createMockProject();
+	return applicationFormatter.format(project).then((oRes) => {
+		t.falsy(oRes);
+		t.pass();
+	});
+});
+
+test("ApplicationFormatter#format: replace id", async (t) => {
+	const applicationFormatter = new ApplicationFormatter();
+	sinon.stub(applicationFormatter, "validate").resolves();
+	sinon.stub(applicationFormatter, "readManifest").resolves({"sap.app": {"id": "my.id"}});
+	const project = createMockProject();
+	return applicationFormatter.format(project).then((oRes) => {
+		t.falsy(oRes);
+		t.is(project.metadata.namespace, "my/id");
 		t.pass();
 	});
 });
