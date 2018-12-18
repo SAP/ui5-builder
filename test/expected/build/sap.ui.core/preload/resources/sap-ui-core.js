87d8656d0a33d6cf962a3a94425f378403400209
Tobias Sorn <tobias.sorn@sap.com>
2018-12-18 14:48:53 +0100

    Adds libraryPreload tests
    Covers sap/ui/core specifics

---
+++ test/expected/build/sap.ui.core/preload/resources/sap-ui-core.js
@@ -0,0 +1,9 @@
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"sap/ui/core/Core.js":function(){
+}
+}});
+sap.ui.requireSync("sap/ui/core/Core");
+// as this module contains the Core, we ensure that the Core has been booted
+sap.ui.getCore().boot && sap.ui.getCore().boot();
---
+++ test/expected/build/sap.ui.core/preload/resources/sap/ui/core/library-preload.js
@@ -0,0 +1,11 @@
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"sap/ui/core/Core.js":function(){
+},
+	"sap/ui/core/some.js":function(){/*!
+ * ${copyright}
+ */
+console.log("HelloWorld");
+}
+}});
