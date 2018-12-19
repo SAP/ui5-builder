9ab469d35ec7e68243db8b7f0ecd6b99118a82b4
Tobias Sorn <tobias.sorn@sap.com>
2018-12-19 15:49:17 +0100

    Improves AutoSplitter test

--- test/lib/lbt/bundle/AutoSplitter.js
+++ test/lib/lbt/bundle/AutoSplitter.js
@@ -5,19 +5,34 @@ const AutoSplitter = require("../../../../lib/lbt/bundle/AutoSplitter");
 
 const ModuleInfo = require("../../../../lib/lbt/resources/ModuleInfo");
 
-function createMockPool(dependency) {
+function createMockPool(dependencies) {
 	return {
 		async findResourceWithInfo(name) {
 			const info = new ModuleInfo(name);
-			info.addDependency(dependency);
-			return {info};
+			dependencies.forEach((dependency) => {
+				info.addDependency(dependency);
+			});
+			if (name === "c.js") {
+				info.compressedSize = 10;
+			}
+			return {info, buffer: () => name};
 		},
-		resources: []
+		resources: [{
+			name: "a.js"
+		}, {
+			name: "c.js"
+		}, {
+			name: "b.json"
+		}, {
+			name: "x.view.xml"
+		}, {
+			name: "c.properties"
+		}]
 	};
 }
 
 test("AutoSplitter 1 part", async (t) => {
-	const pool = createMockPool("mydep");
+	const pool = createMockPool(["mydep"]);
 	const autoSplitter = new AutoSplitter(pool, new BundleResolver(pool));
 	const bundleDefinition = {
 		name: `Component-preload.js`,
@@ -43,40 +58,52 @@ test("AutoSplitter 1 part", async (t) => {
 });
 
 test("AutoSplitter 2 parts", async (t) => {
-	const pool = createMockPool("mydep");
+	const pool = createMockPool(["a.js", "b.json"]);
 	const autoSplitter = new AutoSplitter(pool, new BundleResolver(pool));
 	const bundleDefinition = {
 		name: `Component-preload.js`,
 		defaultFileTypes: [".js", ".fragment.xml", ".view.xml", ".properties", ".json"],
 		sections: [{
 			mode: "preload",
-			filters: [],
+			filters: ["a.js", "c.js", "b.json", "c.properties", "x.view.xml"],
 			resolve: false,
 			resolveConditional: false,
 			renderer: false,
-			modules: ["a", "b"]
+			modules: ["a.js", "c.js", "b.json", "c.properties", "x.view.xml"]
 		}, {
-			mode: "Provided"
+			mode: "provided"
 		}, {
-			mode: "Raw",
-			modules: ["a", "b"],
+			mode: "raw",
+			modules: ["a.js", "c.js", "b.json"],
+			filters: ["a.js", "c.js", "b.json"],
 			sectionDefinition: {
 				declareRawModules: [],
 				sort: false
 			}
 		}, {
-			mode: "Require",
-			modules: ["a", "b"]
-		}]
+			mode: "require",
+			filters: ["a.js", "c.js"],
+			modules: ["a.js", "c.js"]
+		}],
+		configuration: {}
 	};
-	const oResult = await autoSplitter.run(bundleDefinition, {numberOfParts: 2});
+	const oResult = await autoSplitter.run(bundleDefinition, {numberOfParts: 2, optimize: false});
 	t.is(oResult.length, 1);
 	t.deepEqual(oResult[0], {
 		name: `Component-preload-0.js`,
 		sections: [{
 			mode: "preload",
-			filters: [],
+			filters: ["a.js", "c.js", "b.json", "x.view.xml", "c.properties"],
 			name: undefined
-		}]
+		}, {
+			declareRawModules: undefined,
+			mode: "raw",
+			filters: [],
+			sort: undefined
+		}, {
+			mode: "require",
+			filters: ["a.js", "c.js"]
+		}],
+		configuration: {}
 	});
 });
