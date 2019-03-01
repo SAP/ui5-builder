c26f2dee68e9b514a7497458f4210163f0568613
Merlin Beutlberger <m.beutlberger@sap.com>
2019-03-01 15:07:48 +0100

    Add generateJsdoc test file

---
+++ test/lib/tasks/generateJsdoc.js
@@ -0,0 +1,9 @@
+const {test} = require("ava");
+const sinon = require("sinon");
+const mock = require("mock-require");
+
+const generateJsdoc = require("../../../lib/tasks/generateJsdoc");
+
+test("jsdocGenerator", async (t) => {
+
+});
