a4e83383c7371cdde8573a901fdadd2ab243440e
Dario Zilocchi <dario.zilocchi@gmail.com>
2019-04-18 20:59:26 +0200

    [FEATURE] Add option to use hash signatures in cachebuster info file
    - There are situations where using timestamps may not be reliable
      (ex: CI environments)
    - Provide two different signature type to be used: time and hash
    - Read configuration parameter from ui5.yaml file, keeping time as default one

--- test/lib/tasks/generateCachebusterInfo.js
+++ test/lib/tasks/generateCachebusterInfo.js
@@ -57,12 +57,75 @@ test("integration: Build application.g with manifestBundler", (t) => {
 	});
 });
 
+test("integration: Build application.g with manifestBundler and cachebuster using hashes", (t) => {
+	const destPath = path.join("test", "tmp", "build", "application.g", "cachebuster_hash");
+	const expectedPath = path.join("test", "expected", "build", "application.g", "cachebuster");
+	const excludedTasks = ["generateVersionInfo"];
+	const includedTasks = ["generateCachebusterInfo"];
+
+	return builder.build({
+		tree: applicationGTreeWithCachebusterHash,
+		destPath,
+		excludedTasks,
+		includedTasks
+	}).then(() => {
+		return findFiles(expectedPath);
+	}).then((expectedFiles) => {
+		// Check for all directories and files
+		assert.directoryDeepEqual(destPath, expectedPath);
+
+		// Check for all file contents
+		expectedFiles.forEach((expectedFile) => {
+			const relativeFile = path.relative(expectedPath, expectedFile);
+			const destFile = path.join(destPath, relativeFile);
+			if (expectedFile.endsWith("sap-ui-cachebuster-info.json")) {
+				const currentContent = JSON.parse(fs.readFileSync(destFile, "utf-8").replace(/(:\s+)("[^"]+")/g, ": \"\""));
+				const expectedContent = JSON.parse(fs.readFileSync(expectedFile, "utf-8").replace(/(:\s+)(\d+)/g, ": \"\""));
+				assert.deepEqual(currentContent, expectedContent);
+			} else {
+				assert.fileEqual(destFile, expectedFile);
+			}
+		});
+		t.pass();
+	});
+});
+
 const applicationGTree = {
 	"id": "application.g",
 	"version": "1.0.0",
 	"path": applicationGPath,
 	"dependencies": [],
 	"builder": {},
+	"_level": 0,
+	"specVersion": "0.1",
+	"type": "application",
+	"metadata": {
+		"name": "application.g",
+		"namespace": "application.g",
+		"copyright": "Some fancy copyright"
+	},
+	"resources": {
+		"configuration": {
+			"paths": {
+				"webapp": "webapp"
+			}
+		},
+		"pathMappings": {
+			"/": "webapp"
+		}
+	}
+};
+
+const applicationGTreeWithCachebusterHash = {
+	"id": "application.g",
+	"version": "1.0.0",
+	"path": applicationGPath,
+	"dependencies": [],
+	"builder": {
+		"cachebuster": {
+			"signatureType": "hash"
+		}
+	},
 	"_level": 0,
 	"specVersion": "0.1",
 	"type": "application",
