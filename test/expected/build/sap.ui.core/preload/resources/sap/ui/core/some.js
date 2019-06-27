bd7aa409be340216a88ceb2607e85d951c9de58a
Frank Weigel <frank.weigel@sap.com>
2019-06-27 13:04:03 +0200

    [FIX] Use consistent RegExp to detect copyright comments (#275)
    Bundle Builder and AutoSplitter now use the same RegExp to detect copyrights that the uglifier task uses.
    
    Fixes https://github.com/SAP/ui5-builder/issues/257 .

--- test/expected/build/sap.ui.core/preload/resources/sap/ui/core/some.js
+++ test/expected/build/sap.ui.core/preload/resources/sap/ui/core/some.js
@@ -2,3 +2,13 @@
  * ${copyright}
  */
 console.log('HelloWorld');
+
+/*
+ * function add(c) - this is still not implemented.
+ * @private
+ * @param {Component} c
+ * @name add
+ * This comment should be removed in the preload bundle although it contains the magic
+ * sequence "opening parenthesis - letter c - closing parenthesis" which usually indicates
+ * a  c o p y r i g h t  comment.
+ */
