3a33f18664869da9058e13b045a96d38f042797b
Nicolas Lunet <nicolas.lunet@sap.com>
2019-07-09 16:02:59 +0200

    - Also include component preload map

---
+++ test/expected/build/application.g/dest2/Component-preload.js
@@ -0,0 +1,8 @@
+//@ui5-bundle application/g/Component-preload.js
+sap.ui.predefine("application/g/Component",["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.Component",{metadata:{manifest:"json"}})});
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"application/g/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.g","type":"application","applicationVersion":{"version":"1.0.0"},"embeds":["embedded"],"title":"{{title}}"},"customCopyrightString":"Some fancy copyright"}'
+}});
+//# sourceMappingURL=Component-preload.js.map
---
+++ test/expected/build/application.g/dest2/subcomponentB/Component-preload.js
@@ -0,0 +1,8 @@
+//@ui5-bundle application/g/subcomponentB/Component-preload.js
+sap.ui.predefine("application/g/subcomponentB/Component",["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.subcomponentB.Component",{metadata:{manifest:"json"}})});
+jQuery.sap.registerPreloadedModules({
+"version":"2.0",
+"modules":{
+	"application/g/subcomponentB/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.g.subcomponentB","type":"application","applicationVersion":{"version":"1.2.2"},"embeds":["embedded"],"title":"{{title}}"}}'
+}});
+//# sourceMappingURL=Component-preload.js.map
