e84521551b99b3bd9e8f4175e8a280a4281a42f7
Nicolas Lunet <nicolas.lunet@sap.com>
2019-09-19 16:31:23 +0200

    - fix tests

--- test/expected/build/library.d/preload/resources/library/d/library-preload.js
+++ test/expected/build/library.d/preload/resources/library/d/library-preload.js
@@ -1,5 +1,6 @@
 //@ui5-bundle library/d/library-preload.js
 jQuery.sap.registerPreloadedModules({
+"name":"library/d/library-preload",
 "version":"2.0",
 "modules":{
 	"library/d/some.js":function(){/*!
