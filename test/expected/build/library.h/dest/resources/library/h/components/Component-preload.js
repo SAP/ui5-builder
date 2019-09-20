82fe2675114c13603238889e43be498f92d22a51
Merlin Beutlberger <m.beutlberger@sap.com>
2019-09-20 12:45:46 +0200

    [FIX] manifestCreator: Only consider component files called Component.js (#273)
    In the past files called *Component.js where also considered as
    Components during manifest creation.

--- test/expected/build/library.h/dest/resources/library/h/components/Component-preload.js
+++ test/expected/build/library.h/dest/resources/library/h/components/Component-preload.js
@@ -1,4 +1,9 @@
 sap.ui.require.preload({
 	"library/h/components/Component.js":function(){sap.ui.define(["sap/ui/core/UIComponent"],function(n){"use strict";return n.extend("application.g.Component",{})});
+},
+	"library/h/components/TodoComponent.js":function(){/*!
+ * Some fancy copyright
+ */
+console.log(" File ");
 }
 });
