0316e912c811797760012154a232a025e67b91bc
Merlin Beutlberger <m.beutlberger@sap.com>
2020-03-28 00:09:47 +0100

    [INTERNAL] lbt escapePropertiesFile: Expect *.properties files in UTF-8 by default
    If the project a "*.properties" resource originates from cannot be
    determined, or if the project does not define a
    propertiesFileSourceEncoding configuration or uses a legacy specVersion
    (<2.0), escapePropertiesFile assumes that the resource is UTF-8
    encoded instead of ISO-8859-1.
    
    This is an internal module that *should* be wrapped by APIs that enforce
    proper projects which always define a propertiesFileSourceEncoding
    configuration. Therefore this change is assumed to not be breaking.

---
+++ test/lib/lbt/utils/escapePropertiesFile.js
@@ -0,0 +1,212 @@
+const test = require("ava");
+const mock = require("mock-require");
+const sinon = require("sinon");
+
+
+test.beforeEach((t) => {
+	// Spying logger of processors/bootstrapHtmlTransformer
+	t.context.getEncodingFromAliasStub = sinon.stub().returns("node encoding name");
+	t.context.nonAsciiEscaperStub = sinon.stub().resolves();
+	t.context.nonAsciiEscaperStub.getEncodingFromAlias = t.context.getEncodingFromAliasStub;
+	mock("../../../../lib/processors/nonAsciiEscaper", t.context.nonAsciiEscaperStub);
+	t.context.escapePropertiesFile = mock.reRequire("../../../../lib/lbt/utils/escapePropertiesFile");
+});
+
+test.afterEach.always((t) => {
+	sinon.restore();
+	mock.stopAll();
+});
+
+test.serial("propertiesFileSourceEncoding UTF-8", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				resources: {
+					configuration: {
+						propertiesFileSourceEncoding: "UTF-8"
+					}
+				}
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "UTF-8",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
+
+
+test.serial("propertiesFileSourceEncoding ISO-8859-1", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				resources: {
+					configuration: {
+						propertiesFileSourceEncoding: "ISO-8859-1"
+					}
+				}
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "ISO-8859-1",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
+
+test.serial("propertiesFileSourceEncoding not set", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				resources: {
+					configuration: {
+					}
+				}
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "UTF-8",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
+
+test.serial("propertiesFileSourceEncoding not set - specVersion 0.1", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				specVersion: "0.1"
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "ISO-8859-1",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
+
+test.serial("propertiesFileSourceEncoding not set - specVersion 1.0", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				specVersion: "1.0"
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "ISO-8859-1",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
+
+test.serial("propertiesFileSourceEncoding not set - specVersion 1.1", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				specVersion: "1.1"
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "ISO-8859-1",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
+
+test.serial("propertiesFileSourceEncoding not set - specVersion 2.0", async (t) => {
+	const lbtResource = {
+		getProject: () => {
+			return {
+				specVersion: "2.0"
+			};
+		},
+		resource: "actual resource",
+		buffer: async () => {
+			return "resource content";
+		}
+	};
+	const res = await t.context.escapePropertiesFile(lbtResource);
+	t.is(t.context.getEncodingFromAliasStub.callCount, 1, "getEncodingFromAlias got called once");
+	t.is(t.context.getEncodingFromAliasStub.getCall(0).args[0], "UTF-8",
+		"getEncodingFromAlias got called with excepted argument");
+	t.is(t.context.nonAsciiEscaperStub.callCount, 1, "nonAsciiEscaper got called once");
+	t.deepEqual(t.context.nonAsciiEscaperStub.getCall(0).args[0], {
+		resources: ["actual resource"],
+		options: {
+			encoding: "node encoding name"
+		}
+	}, "getEncodingFromAlias got called with excepted argument");
+	t.is(res, "resource content", "Correct result");
+});
