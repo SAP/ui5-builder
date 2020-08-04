71d0c58064fdeddd11eb61d34b6ef51fcca85d00
Tobias Sorn <tobias.sorn@sap.com>
2020-08-04 13:50:19 +0200

    improved failOnOrphans error

--- test/lib/processors/resourceListCreator.js
+++ test/lib/processors/resourceListCreator.js
@@ -1,7 +1,47 @@
 const test = require("ava");
+const sinon = require("sinon");
+const mock = require("mock-require");
 
-const resourceListCreator = require("../../../lib/processors/resourceListCreator");
+let resourceListCreator = require("../../../lib/processors/resourceListCreator");
+const resourceFactory = require("@ui5/fs").resourceFactory;
 
-test("Replaces string pattern from resource stream", async (t) => {
-	t.truthy(resourceListCreator);
+test.beforeEach((t) => {
+	// Spying logger of processors/bootstrapHtmlTransformer
+	const log = require("@ui5/logger");
+	const loggerInstance = log.getLogger("builder:processors:resourceListCreator");
+	mock("@ui5/logger", {
+		getLogger: () => loggerInstance
+	});
+	mock.reRequire("@ui5/logger");
+	t.context.logErrorSpy = sinon.spy(loggerInstance, "error");
+
+	// Re-require tested module
+	resourceListCreator = mock.reRequire("../../../lib/processors/resourceListCreator");
+});
+
+test.afterEach.always((t) => {
+	mock.stop("@ui5/logger");
+	t.context.logErrorSpy.restore();
+});
+
+test.serial("Empty resources", async (t) => {
+	const result = await resourceListCreator({
+		resources: []
+	});
+	t.deepEqual(result, []);
+});
+
+test.serial("Orphaned resources", async (t) => {
+	const resource = resourceFactory.createResource({
+		path: "/resources/nomodule.foo",
+		string: "bar content"
+	});
+	const errorObject = await t.throwsAsync(() => {
+		return resourceListCreator({
+			resources: [resource]
+		});
+	});
+	t.is(errorObject.message, "resources.json generation failed with error: There are 1 resources which could not be assigned to components.");
+	t.is(t.context.logErrorSpy.callCount, 1);
+	t.is(t.context.logErrorSpy.getCall(0).args[0], "resources.json generation failed because of unassigned resources: nomodule.foo");
 });
