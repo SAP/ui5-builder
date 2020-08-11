d0190bb706effe27f4c9aede3bf7e0096f74af66
Tobias Sorn <tobias.sorn@sap.com>
2020-08-11 16:43:45 +0200

    Add additional ModuleInfo tests

--- test/lib/lbt/resources/ModuleInfo.js
+++ test/lib/lbt/resources/ModuleInfo.js
@@ -29,3 +29,35 @@ test("ModuleInfo: addSubModule", async (t) => {
 	t.deepEqual(moduleInfo.subModules, ["otherModule"], "submodule is set");
 	t.deepEqual(moduleInfo.dependencies, ["unknownModule"], "unknownModule dependency is copied over");
 });
+
+test("ModuleInfo: name", async (t) => {
+	// setup
+	const moduleInfo = new ModuleInfo("myName");
+
+	// action
+	moduleInfo.addDependency("newName", false);
+	moduleInfo.name = "newName";
+
+	moduleInfo.addSubModule("newName2");
+	moduleInfo.name = "newName2";
+
+	// expectation
+	t.deepEqual(moduleInfo.subModules, [], "submodule is empty");
+	t.deepEqual(moduleInfo.dependencies, [], "dependencies is empty");
+	t.is(moduleInfo.name, "newName2", "name was set");
+});
+
+test("ModuleInfo: toString", async (t) => {
+	// setup
+	const moduleInfo = new ModuleInfo("myName");
+
+	// action
+	moduleInfo.addDependency("dep1", false);
+	moduleInfo.addDependency("dep2", false);
+	moduleInfo.addSubModule("sub1");
+	moduleInfo.addSubModule("sub2");
+	const stringContent = moduleInfo.toString();
+
+	// expectation
+	t.is(stringContent, "ModuleInfo(myName, dependencies=dep1,dep2, includes=sub1,sub2)", "string value is correct");
+});
