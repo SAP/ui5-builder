b5777cc0a497cbeb5644f54b599843b74d81e260
Tobias Sorn <tobias.sorn@sap.com>
2020-08-20 15:02:38 +0200

    [INTERNAL] add tests
    fix naming in manifest.json file of library H

---
+++ test/lib/types/themeLibrary/ThemeLibraryBuilder.js
@@ -0,0 +1,46 @@
+const test = require("ava");
+
+const ThemeLibraryBuilder = require("../../../../lib/types/themeLibrary/ThemeLibraryBuilder");
+
+test("tasks", async (t) => {
+	const themeLibraryBuilder = new ThemeLibraryBuilder({
+		resourceCollections: {
+			workspace: {
+				byGlob: async () => {
+					return [];
+				}
+			}
+		},
+		buildContext: {
+			isRootProject: () => {
+				return true;
+			}
+		},
+		project: {
+			metadata: {
+				name: "name",
+				copyright: "copyright"
+			},
+			type: "type"
+		},
+		parentLogger: {
+			createSubLogger: () => {
+				return {
+					createTaskLogger: () => {
+
+					}
+				};
+			}
+		},
+		taskUtil: {
+			isRootProject: () => {
+				return true;
+			}
+		}
+	});
+	const asyncTasks = Object.keys(themeLibraryBuilder.tasks).map((taskKey) => {
+		return themeLibraryBuilder.tasks[taskKey]();
+	});
+
+	t.is(asyncTasks.length, 3, "all 3 tasks should be added");
+});
