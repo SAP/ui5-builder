e3d36d1dd3b6d6f819cd430d56c86ab8c3b8028c
Merlin Beutlberger <m.beutlberger@sap.com>
2020-11-04 17:47:52 +0100

    Revert "[FEATURE] Switch XML minifier from pretty-data to minify-xml"
    This partially reverts commit 198dc2c4d698892bd9cc3f86709d32c5cd5698b1.
    
    Resolves https://github.com/SAP/ui5-cli/issues/385
    Related to https://github.com/SAP/ui5-builder/issues/538

--- test/expected/build/application.i/dest/Component-preload.js
+++ test/expected/build/application.i/dest/Component-preload.js
@@ -7,6 +7,6 @@ jQuery.sap.registerPreloadedModules({
 	"application/i/changes/changes-bundle.json":'[{"fileName":"id_456_addField","fileType":"change","changeType":"hideControl","component":"application.i.Component","content":{},"selector":{"id":"control1"},"layer":"VENDOR","texts":{},"namespace":"apps/application.i.Component/changes","creation":"2023-10-30T13:52:40.4754350Z","originalLanguage":"","conditions":{},"support":{"generator":"did it","user":"Max Mustermann"}},{"fileName":"id_123_addField","fileType":"change","changeType":"hideControl","component":"application.i.Component","content":{},"selector":{"id":"control1"},"layer":"VENDOR","texts":{},"namespace":"apps/application.i.Component/changes","creation":"2025-10-30T13:52:40.4754350Z","originalLanguage":"","conditions":{},"support":{"generator":"did it","user":"Max Mustermann"}}]',
 	"application/i/changes/coding/MyExtension.js":function(){sap.ui.define([],function(){return{}});
 },
-	"application/i/changes/fragments/MyFragment.fragment.xml":'<xml/>',
+	"application/i/changes/fragments/MyFragment.fragment.xml":'<xml></xml>',
 	"application/i/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"application.i","type":"application","applicationVersion":{"version":"1.2.2"},"embeds":["embedded"],"title":"{{title}}"},"sap.ui5":{"dependencies":{"libs":{"sap.ui.layout":{},"sap.ui.core":{},"sap.m":{},"sap.ui.fl":{}}}}}'
 }});
