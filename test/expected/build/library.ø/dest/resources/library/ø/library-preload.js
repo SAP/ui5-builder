b561ea733399b2822340d2ed616560588e2fde2f
Tobias Sorn <tobias.sorn@sap.com>
2020-12-23 10:25:26 +0100

    [FEATURE] manifestCreator: i18n section v22
    Adjust test code

--- test/expected/build/library.ø/dest/resources/library/ø/library-preload.js
+++ test/expected/build/library.ø/dest/resources/library/ø/library-preload.js
@@ -1,6 +1,6 @@
 //@ui5-bundle library/ø/library-preload.js
 sap.ui.require.preload({
-	"library/ø/manifest.json":'{"_version":"1.9.0","sap.app":{"id":"library.ø","type":"library","embeds":[],"applicationVersion":{"version":"1.0.0"},"title":"Library Ø","description":"Library Ø","resources":"resources.json","offline":true},"sap.ui":{"technology":"UI5","supportedThemes":["цветя"]},"sap.ui5":{"dependencies":{"minUI5Version":"1.0","libs":{}},"library":{"i18n":false}}}',
+	"library/ø/manifest.json":'{"_version":"1.21.0","sap.app":{"id":"library.ø","type":"library","embeds":[],"applicationVersion":{"version":"1.0.0"},"title":"Library Ø","description":"Library Ø","resources":"resources.json","offline":true},"sap.ui":{"technology":"UI5","supportedThemes":["цветя"]},"sap.ui5":{"dependencies":{"minUI5Version":"1.0","libs":{}},"library":{"i18n":false}}}',
 	"library/ø/some.js":function(){/*!
  * Some fancy copyright
  */
