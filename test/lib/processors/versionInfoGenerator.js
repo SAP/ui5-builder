b9da72db5dec100900c85b27ce69c2cee73a1a88
Tobias Sorn <tobias.sorn@sap.com>
2021-01-19 10:54:55 +0100

    improve jsdoc and reuse types
    add tests for versionInfoGenerator with new parameters
    * libraryManifest
    * embeddedManifests

--- test/lib/processors/versionInfoGenerator.js
+++ test/lib/processors/versionInfoGenerator.js
@@ -14,8 +14,12 @@ test("versionInfoGenerator missing parameters", async (t) => {
 
 test.beforeEach((t) => {
 	t.context.warnLogStub = sinon.stub();
+	t.context.infoLogStub = sinon.stub();
+	t.context.verboseLogStub = sinon.stub();
 	sinon.stub(logger, "getLogger").returns({
 		warn: t.context.warnLogStub,
+		info: t.context.infoLogStub,
+		verbose: t.context.verboseLogStub,
 		isLevelEnabled: () => true
 	});
 	versionInfoGenerator = mock.reRequire("../../../lib/processors/versionInfoGenerator");
@@ -84,4 +88,233 @@ test.serial("versionInfoGenerator simple library infos", async (t) => {
 	t.is(t.context.warnLogStub.callCount, 1);
 	t.is(t.context.warnLogStub.getCall(0).args[0],
 		"Cannot add meta information for library 'my.lib'. The manifest.json file cannot be found");
+});
+
+test.serial("versionInfoGenerator library infos with dependencies", async (t) => {
+	const libAManifest = {
+		getPath: () => {
+			return "/resources/lib/a/manifest.json";
+		},
+		getString: async () => {
+			return JSON.stringify({
+				"sap.app": {
+					"id": "lib.a",
+					"embeds": []
+				},
+				"sap.ui5": {
+					"dependencies": {
+						"minUI5Version": "1.84",
+						"libs": {
+							"my.dep": {
+								"minVersion": "1.84.0"
+							}
+						}
+					}
+				}
+			});
+		}
+	};
+	const libA = {name: "lib.a", version: "1.2.3", libraryManifest: libAManifest};
+	const myDepManifest = {
+		getPath: () => {
+			return "/resources/my/dep/manifest.json";
+		},
+		getString: async () => {
+			return JSON.stringify({
+				"sap.app": {
+					"id": "my.dep",
+					"embeds": []
+				},
+				"sap.ui5": {
+					"dependencies": {
+						"minUI5Version": "1.84",
+						"libs": {}
+					}
+				}
+			});
+		}
+	};
+	const myDep = {name: "my.dep", version: "1.2.3", libraryManifest: myDepManifest};
+	const options = {
+		rootProjectName: "myname", rootProjectVersion: "1.33.7", libraryInfos: [
+			libA, myDep
+		]};
+	const versionInfos = await versionInfoGenerator({options});
+
+	const resource = versionInfos[0];
+	const result = await resource.getString();
+
+	const oExpected = {
+		"name": "myname",
+		"version": "1.33.7",
+		"scmRevision": "",
+		"libraries": [
+			{
+				"name": "lib.a",
+				"version": "1.2.3",
+				"scmRevision": "",
+				"manifestHints": {
+					"dependencies": {
+						"libs": {
+							"my.dep": {}
+						}
+					}
+				}
+			},
+			{
+				"name": "my.dep",
+				"version": "1.2.3",
+				"scmRevision": ""
+			}
+		]
+	};
+	assertVersionInfoContent(t, oExpected, result);
+	t.is(t.context.infoLogStub.callCount, 0);
+	t.is(t.context.warnLogStub.callCount, 0);
+});
+
+test.serial("versionInfoGenerator library infos with embeds", async (t) => {
+	const libAManifest = {
+		getPath: () => {
+			return "/resources/lib/a/manifest.json";
+		},
+		getString: async () => {
+			return JSON.stringify({
+				"sap.app": {
+					"id": "lib.a",
+					"embeds": ["sub"]
+				},
+				"sap.ui5": {
+					"dependencies": {
+						"minUI5Version": "1.84",
+						"libs": {}
+					}
+				}
+			});
+		}
+	};
+	const subManifest = {
+		getPath: () => {
+			return "/resources/lib/a/sub/manifest.json";
+		},
+		getString: async () => {
+			return JSON.stringify({
+				"sap.app": {
+					"id": "lib.a.sub",
+					"embeds": []
+				},
+				"sap.ui5": {
+					"dependencies": {
+						"minUI5Version": "1.84",
+						"libs": {}
+					}
+				}
+			});
+		}
+	};
+	const libA = {name: "lib.a", version: "1.2.3", libraryManifest: libAManifest, embeddedManifests: [subManifest]};
+
+	const options = {
+		rootProjectName: "myname", rootProjectVersion: "1.33.7", libraryInfos: [
+			libA
+		]};
+	const versionInfos = await versionInfoGenerator({options});
+
+	const resource = versionInfos[0];
+	const result = await resource.getString();
+
+	const oExpected = {
+		"name": "myname",
+		"version": "1.33.7",
+		"scmRevision": "",
+		"libraries": [
+			{
+				"name": "lib.a",
+				"version": "1.2.3",
+				"scmRevision": ""
+			}
+		],
+		"components": {
+			"lib.a.sub": {
+				"library": "lib.a"
+			}
+		}
+	};
+	assertVersionInfoContent(t, oExpected, result);
+	t.is(t.context.infoLogStub.callCount, 0);
+	t.is(t.context.warnLogStub.callCount, 0);
+});
+
+test.serial("versionInfoGenerator library infos with embeds and embeddedBy (hasOwnPreload)", async (t) => {
+	const libAManifest = {
+		getPath: () => {
+			return "/resources/lib/a/manifest.json";
+		},
+		getString: async () => {
+			return JSON.stringify({
+				"sap.app": {
+					"id": "lib.a",
+					"embeds": ["sub"]
+				},
+				"sap.ui5": {
+					"dependencies": {
+						"minUI5Version": "1.84",
+						"libs": {}
+					}
+				}
+			});
+		}
+	};
+	const subManifest = {
+		getPath: () => {
+			return "/resources/lib/a/sub/manifest.json";
+		},
+		getString: async () => {
+			return JSON.stringify({
+				"sap.app": {
+					"id": "lib.a.sub",
+					"embeds": [],
+					"embeddedBy": "../"
+				},
+				"sap.ui5": {
+					"dependencies": {
+						"minUI5Version": "1.84",
+						"libs": {}
+					}
+				}
+			});
+		}
+	};
+	const libA = {name: "lib.a", version: "1.2.3", libraryManifest: libAManifest, embeddedManifests: [subManifest]};
+
+	const options = {
+		rootProjectName: "myname", rootProjectVersion: "1.33.7", libraryInfos: [
+			libA
+		]};
+	const versionInfos = await versionInfoGenerator({options});
+
+	const resource = versionInfos[0];
+	const result = await resource.getString();
+
+	const oExpected = {
+		"name": "myname",
+		"version": "1.33.7",
+		"scmRevision": "",
+		"libraries": [
+			{
+				"name": "lib.a",
+				"version": "1.2.3",
+				"scmRevision": ""
+			}
+		],
+		"components": {
+			"lib.a.sub": {
+				"hasOwnPreload": true,
+				"library": "lib.a"
+			}
+		}
+	};
+	assertVersionInfoContent(t, oExpected, result);
+	t.is(t.context.infoLogStub.callCount, 0);
+	t.is(t.context.warnLogStub.callCount, 0);
 });
