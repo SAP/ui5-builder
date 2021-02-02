2e3fae1e76ed0bb80b0e918234ebf10a45201d61
Matthias Osswald <mat.osswald@sap.com>
2021-02-02 14:32:49 +0100

    Fix WIP code in generateComponentPreload / add test cases

--- test/lib/tasks/bundlers/generateComponentPreload.js
+++ test/lib/tasks/bundlers/generateComponentPreload.js
@@ -29,7 +29,7 @@ test.afterEach.always(() => {
 	mock.stopAll();
 });
 
-test.serial("generateComponentPreload", async (t) => {
+test.serial("generateComponentPreload - one namespace", async (t) => {
 	const {
 		generateComponentPreload, moduleBundlerStub, ReaderCollectionPrioritizedStub,
 		workspace, dependencies, comboByGlob
@@ -99,5 +99,209 @@ test.serial("generateComponentPreload", async (t) => {
 	t.deepEqual(workspace.write.getCall(0).args, [bundleResources[0]],
 		"workspace.write should have been called with expected args");
 	t.is(workspace.write.getCall(0).args[0], bundleResources[0],
+		"workspace.write should have been called with exact resource returned by moduleBundler");
+});
+
+test.serial("generateComponentPreload - one namespace - excludes", async (t) => {
+	const {
+		generateComponentPreload, moduleBundlerStub, ReaderCollectionPrioritizedStub,
+		workspace, dependencies, comboByGlob
+	} = t.context;
+
+	const resources = [
+		{"fake": "resource"}
+	];
+	comboByGlob.resolves(resources);
+
+	await generateComponentPreload({
+		workspace,
+		dependencies,
+		options: {
+			projectName: "Test Application",
+			namespaces: ["my/app"],
+			excludes: [
+				"my/app/thirdparty/",
+				"!my/app/thirdparty/NotExcluded.js"
+			]
+		}
+	});
+
+	t.is(moduleBundlerStub.callCount, 1, "moduleBundler should have been called once");
+	t.deepEqual(moduleBundlerStub.getCall(0).args, [{
+		options: {
+			bundleDefinition: {
+				defaultFileTypes: [
+					".js",
+					".fragment.xml",
+					".view.xml",
+					".properties",
+					".json",
+				],
+				name: "my/app/Component-preload.js",
+				sections: [
+					{
+						filters: [
+							"my/app/",
+							"!my/app/test/",
+							"!my/app/*.html",
+							"!my/app/thirdparty/",
+							"my/app/thirdparty/NotExcluded.js"
+						],
+						mode: "preload",
+						renderer: false,
+						resolve: false,
+						resolveConditional: false,
+					}
+				]
+			},
+			bundleOptions: {
+				optimize: true,
+				ignoreMissingModules: true
+			}
+		},
+		resources
+	}]);
+
+	t.is(comboByGlob.callCount, 1,
+		"combo.byGlob should have been called once");
+	t.deepEqual(comboByGlob.getCall(0).args, ["/resources/**/*.{js,json,xml,html,properties,library}"],
+		"combo.byGlob should have been called with expected pattern");
+
+	t.is(ReaderCollectionPrioritizedStub.callCount, 1,
+		"ReaderCollectionPrioritized should have been called once");
+	t.true(ReaderCollectionPrioritizedStub.calledWithNew(),
+		"ReaderCollectionPrioritized should have been called with 'new'");
+
+	const bundleResources = await moduleBundlerStub.getCall(0).returnValue;
+	t.is(workspace.write.callCount, 1,
+		"workspace.write should have been called once");
+	t.deepEqual(workspace.write.getCall(0).args, [bundleResources[0]],
+		"workspace.write should have been called with expected args");
+	t.is(workspace.write.getCall(0).args[0], bundleResources[0],
+		"workspace.write should have been called with exact resource returned by moduleBundler");
+});
+
+test.serial("generateComponentPreload - multiple namespaces - excludes", async (t) => {
+	const {
+		generateComponentPreload, moduleBundlerStub, ReaderCollectionPrioritizedStub,
+		workspace, dependencies, comboByGlob
+	} = t.context;
+
+	const resources = [
+		{"fake": "resource"}
+	];
+	comboByGlob.resolves(resources);
+
+	await generateComponentPreload({
+		workspace,
+		dependencies,
+		options: {
+			projectName: "Test Application",
+			namespaces: [
+				"my/app1",
+				"my/app2"
+			],
+			excludes: [
+				"my/app1/thirdparty1/",
+				"!my/app1/thirdparty1/NotExcluded.js",
+				"my/app2/thirdparty2/",
+				"!my/app2/thirdparty2/NotExcluded.js"
+			]
+		}
+	});
+
+	t.is(moduleBundlerStub.callCount, 2, "moduleBundler should have been called twice");
+	t.deepEqual(moduleBundlerStub.getCall(0).args, [{
+		options: {
+			bundleDefinition: {
+				defaultFileTypes: [
+					".js",
+					".fragment.xml",
+					".view.xml",
+					".properties",
+					".json",
+				],
+				name: "my/app1/Component-preload.js",
+				sections: [
+					{
+						filters: [
+							"my/app1/",
+							"!my/app1/test/",
+							"!my/app1/*.html",
+							"!my/app1/thirdparty1/",
+							"my/app1/thirdparty1/NotExcluded.js",
+							"!my/app2/thirdparty2/", // TODO: is this really expected?
+						],
+						mode: "preload",
+						renderer: false,
+						resolve: false,
+						resolveConditional: false,
+					}
+				]
+			},
+			bundleOptions: {
+				optimize: true,
+				ignoreMissingModules: true
+			}
+		},
+		resources
+	}]);
+	t.deepEqual(moduleBundlerStub.getCall(1).args, [{
+		options: {
+			bundleDefinition: {
+				defaultFileTypes: [
+					".js",
+					".fragment.xml",
+					".view.xml",
+					".properties",
+					".json",
+				],
+				name: "my/app2/Component-preload.js",
+				sections: [
+					{
+						filters: [
+							"my/app2/",
+							"!my/app2/test/",
+							"!my/app2/*.html",
+							"!my/app1/thirdparty1/", // TODO: is this really expected?
+							"!my/app2/thirdparty2/",
+							"my/app2/thirdparty2/NotExcluded.js"
+						],
+						mode: "preload",
+						renderer: false,
+						resolve: false,
+						resolveConditional: false,
+					}
+				]
+			},
+			bundleOptions: {
+				optimize: true,
+				ignoreMissingModules: true
+			}
+		},
+		resources
+	}]);
+
+	t.is(comboByGlob.callCount, 1,
+		"combo.byGlob should have been called once");
+	t.deepEqual(comboByGlob.getCall(0).args, ["/resources/**/*.{js,json,xml,html,properties,library}"],
+		"combo.byGlob should have been called with expected pattern");
+
+	t.is(ReaderCollectionPrioritizedStub.callCount, 1,
+		"ReaderCollectionPrioritized should have been called once");
+	t.true(ReaderCollectionPrioritizedStub.calledWithNew(),
+		"ReaderCollectionPrioritized should have been called with 'new'");
+
+	const bundleResources1 = await moduleBundlerStub.getCall(0).returnValue;
+	const bundleResources2 = await moduleBundlerStub.getCall(1).returnValue;
+	t.is(workspace.write.callCount, 2,
+		"workspace.write should have been called twice");
+	t.deepEqual(workspace.write.getCall(0).args, [bundleResources1[0]],
+		"workspace.write should have been called with expected args");
+	t.is(workspace.write.getCall(0).args[0], bundleResources1[0],
+		"workspace.write should have been called with exact resource returned by moduleBundler");
+	t.deepEqual(workspace.write.getCall(1).args, [bundleResources2[0]],
+		"workspace.write should have been called with expected args");
+	t.is(workspace.write.getCall(1).args[0], bundleResources2[0],
 		"workspace.write should have been called with exact resource returned by moduleBundler");
 });
