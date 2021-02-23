f98dc260537cb157520d6d105e6bb5dacee5a667
Matthias Osswald <mat.osswald@sap.com>
2021-02-23 18:03:09 +0100

    [FIX] resourceListCreator: Include dependencies of subModules

--- test/lib/processors/resourceListCreator.js
+++ test/lib/processors/resourceListCreator.js
@@ -205,3 +205,105 @@ test.serial("XML View with control resource as dependency", async (t) => {
 	]
 }`);
 });
+
+test.serial("bundle containing an XML View with control resource as dependency", async (t) => {
+	const myAppManifestJsonResource = resourceFactory.createResource({
+		path: "/resources/my/app/manifest.json",
+		string: JSON.stringify({"sap.app": {"id": "my.app"}})
+	});
+	const myAppXmlViewResource = resourceFactory.createResource({
+		path: "/resources/my/app/view/Main.view.xml",
+		string: `<mvc:View
+			controllerName="my.app.controller.Main"
+			xmlns="my.lib"
+			xmlns:myapp="my.app.controls"
+			xmlns:mvc="sap.ui.core.mvc">
+
+			<!-- Existing control, should be listed as "required" -->
+			<Button></Button>
+
+			<!-- Nonexistent control, should not be listed -->
+			<NonexistentControl></NonexistentControl>
+
+			<!-- Existing control within same project (app), should be listed as "required" -->
+			<myapp:Button></myapp:Button>
+
+			<!-- Nonexistent control within same project (app), should not be listed -->
+			<myapp:NonexistentControl></myapp:NonexistentControl>
+
+		</mvc:View>`
+	});
+	// eslint-disable-next-line max-len
+	const bundledXmlView = `<mvc:View controllerName="my.app.controller.Main" xmlns="my.lib" xmlns:myapp="my.app.controls" xmlns:mvc="sap.ui.core.mvc"><Button></Button><NonexistentControl></NonexistentControl><myapp:Button></myapp:Button><myapp:NonexistentControl></myapp:NonexistentControl></mvc:View>`;
+	const myAppBundleResource = resourceFactory.createResource({
+		path: "/resources/my/app/bundle.js",
+		string: `//@ui5-bundle my/app/bundle.js
+sap.ui.require.preload({
+	"my/app/view/Main.view.xml": '${bundledXmlView}'
+});
+`
+	});
+
+	const myAppButtonResource = resourceFactory.createResource({
+		path: "/resources/my/app/controls/Button.js",
+		string: ""
+	});
+	const myLibButtonResource = resourceFactory.createResource({
+		path: "/resources/my/lib/Button.js",
+		string: ""
+	});
+
+	const resourcesJson = await resourceListCreator({
+		resources: [myAppManifestJsonResource, myAppXmlViewResource, myAppButtonResource, myAppBundleResource],
+		dependencyResources: [myLibButtonResource]
+	});
+
+	t.is(resourcesJson.length, 1, "One resources.json should be returned");
+	const myAppResourcesJson = resourcesJson[0];
+	t.is(myAppResourcesJson.getPath(), "/resources/my/app/resources.json");
+	const myAppResourcesJsonContent = await myAppResourcesJson.getString();
+	t.is(myAppResourcesJsonContent, `{
+	"_version": "1.1.0",
+	"resources": [
+		{
+			"name": "bundle.js",
+			"module": "my/app/bundle.js",
+			"size": 367,
+			"merged": true,
+			"required": [
+				"my/app/controller/Main.controller.js",
+				"my/app/controls/Button.js",
+				"my/lib/Button.js"
+			],
+			"included": [
+				"my/app/view/Main.view.xml"
+			]
+		},
+		{
+			"name": "controls/Button.js",
+			"module": "my/app/controls/Button.js",
+			"size": 0,
+			"format": "raw"
+		},
+		{
+			"name": "manifest.json",
+			"module": "my/app/manifest.json",
+			"size": 27
+		},
+		{
+			"name": "resources.json",
+			"size": 801
+		},
+		{
+			"name": "view/Main.view.xml",
+			"module": "my/app/view/Main.view.xml",
+			"size": 592,
+			"required": [
+				"my/app/controller/Main.controller.js",
+				"my/app/controls/Button.js",
+				"my/lib/Button.js"
+			]
+		}
+	]
+}`);
+});
