5ffc47055ea2dce3761ca781e10d41e2e8117217
Tobias Sorn <tobias.sorn@sap.com>
2021-05-20 12:09:16 +0200

    [INTERNAL][WIP] jsdoc: explicitly defined module name
    A warning is raised when the explicitly defined module
    name does not match the resource name.
    
    e.g.
    resource: library/j/customModuleName.js
    defined: library/j/myCustom
    
    JIRA: CPOUI5FOUNDATION-329

---
+++ test/expected/build/library.j/dest/resources/library/j/customModuleName.js
@@ -0,0 +1,18 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define("library/j/myCustom", [],
+	function() {
+	"use strict";
+
+	/**
+	 * @class
+	 * @name library.j.myCustom
+	 * @public
+	 */
+	var customModuleName = {};
+
+	return customModuleName;
+
+});
