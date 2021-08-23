f2b8b57e84ae1a0cc03cc226b430fcacc6b17f0a
Kristian Kraljic <kris@kra.lc>
2021-08-23 13:27:44 +0200

    [FEATURE] Introduce concept of task identifiers to AbstractBuilder
    Task identifiers (in short taskIds) can now be used in AbstractBuilder to define the order of multiple task executions. So far the name of the task (taskName) was treated the identifier of a task. If the same task was added to the execution multiple times, an ID in form taskName--1 was generated, counting upwards from one. In the task definition there was no way of defining the sub-order of multiple executions.
    
    Now with the introduction of task identifiers, one can define a { id: "taskA", ... } in the task definition or call addTask with both an identifier and the name of the task to call. The old logic of generating an identifier, in case only a name is defined stays in place.
    
    The before/afterTask declarations now no longer refer to task names but to task identifiers instead. However due to task names have previously been treated as task identifiers exclusively and also the old incrementation logic for task names stayed the same, this change is compatible with any old configuration.

--- test/lib/types/AbstractBuilder.js
+++ test/lib/types/AbstractBuilder.js
@@ -91,6 +91,10 @@ test("Instantiation with standard tasks only", (t) => {
 	const project = clone(applicationBTree);
 
 	const customBuilder = new CustomBuilder({project});
+	t.deepEqual(customBuilder.taskExecutions, {
+		"myStandardTask": ["myStandardTask"], "createDebugFiles": ["createDebugFiles"],
+		"replaceVersion": ["replaceVersion"]
+	}, "Task executions are correct");
 	t.deepEqual(customBuilder.taskExecutionOrder,
 		["myStandardTask", "createDebugFiles", "replaceVersion"],
 		"Order of tasks is correct");
@@ -185,6 +189,10 @@ test.serial("Instantiation with custom task", (t) => {
 	};
 	const customBuilder = new CustomBuilder({project});
 	t.truthy(customBuilder.tasks["myTask"], "Custom task has been added to task array");
+	t.deepEqual(customBuilder.taskExecutions, {
+		"myStandardTask": ["myStandardTask"], "createDebugFiles": ["createDebugFiles"],
+		"myTask": ["myTask"], "replaceVersion": ["replaceVersion"]
+	}, "Task executions are correct");
 	t.deepEqual(customBuilder.taskExecutionOrder,
 		["myStandardTask", "createDebugFiles", "myTask", "replaceVersion"],
 		"Order of tasks is correct");
@@ -208,6 +216,9 @@ test.serial("Instantiation of empty builder with custom tasks", (t) => {
 	const customBuilder = new EmptyBuilder({project});
 	t.truthy(customBuilder.tasks["myTask"], "Custom task has been added to task array");
 	t.truthy(customBuilder.tasks["myTask2"], "Custom task 2 has been added to task array");
+	t.deepEqual(customBuilder.taskExecutions, {
+		"myTask": ["myTask"], "myTask2": ["myTask2"]
+	}, "Task executions are correct");
 	t.deepEqual(customBuilder.taskExecutionOrder,
 		["myTask2", "myTask"],
 		"Order of tasks is correct");
@@ -298,9 +309,72 @@ test.serial("Instantiation with custom task defined three times", (t) => {
 	t.truthy(customBuilder.tasks["myTask"], "Custom task myTask has been added to task array");
 	t.truthy(customBuilder.tasks["myTask--1"], "Custom task myTask--1 has been added to task array");
 	t.truthy(customBuilder.tasks["myTask--2"], "Custom task myTask--2 has been added to task array");
+	t.deepEqual(customBuilder.taskExecutions, {
+		"myStandardTask": ["myStandardTask"], "createDebugFiles": ["createDebugFiles"],
+		"myTask": ["myTask", "myTask--1", "myTask--2"], "replaceVersion": ["replaceVersion"]
+	}, "Task executions are correct");
 	t.deepEqual(customBuilder.taskExecutionOrder,
 		["myTask", "myTask--2", "myStandardTask", "createDebugFiles", "replaceVersion", "myTask--1"],
 		"Order of tasks is correct");
+});
+
+test.serial("Instantiation with custom task defined three times with ids", (t) => {
+	sinon.stub(taskRepository, "getTask").returns({
+		task: function() {},
+		specVersion: "2.0"
+	});
+
+	const project = clone(applicationBTree);
+	project.builder = {
+		customTasks: [{
+			id: "myTask1",
+			name: "myTask",
+			beforeTask: "myStandardTask"
+		}, {
+			id: "myTask2",
+			name: "myTask",
+			afterTask: "replaceVersion"
+		}, {
+			id: "myTask3",
+			name: "myTask",
+			beforeTask: "myStandardTask"
+		}]
+	};
+	const customBuilder = new CustomBuilder({project});
+	t.truthy(customBuilder.tasks["myTask1"], "Custom task myTask1 has been added to task array");
+	t.truthy(customBuilder.tasks["myTask2"], "Custom task myTask2 has been added to task array");
+	t.truthy(customBuilder.tasks["myTask3"], "Custom task myTask3 has been added to task array");
+	t.deepEqual(customBuilder.taskExecutions, {
+		"myStandardTask": ["myStandardTask"], "createDebugFiles": ["createDebugFiles"],
+		"myTask": ["myTask1", "myTask2", "myTask3"], "replaceVersion": ["replaceVersion"]
+	}, "Task executions are correct");
+	t.deepEqual(customBuilder.taskExecutionOrder,
+		["myTask1", "myTask3", "myStandardTask", "createDebugFiles", "replaceVersion", "myTask2"],
+		"Order of tasks is correct");
+});
+
+test.serial("Instantiation of empty builder with duplicate task ids", (t) => {
+	sinon.stub(taskRepository, "getTask").returns({
+		task: function() {},
+		specVersion: "2.0"
+	});
+
+	const project = clone(applicationBTree);
+	project.builder = {
+		customTasks: [{
+			id: "myTask",
+			name: "myTask"
+		}, {
+			id: "myTask",
+			name: "myTask",
+			afterTask: "myTask"
+		}]
+	};
+	const error = t.throws(() => {
+		new EmptyBuilder({project});
+	});
+	t.deepEqual(error.message, `Conflicting custom task definition myTask of project application.b, more than ` +
+		`one task with the same identifier defined. Task identifiers must be unique.`, "Correct exception thrown");
 });
 
 test.serial("Instantiation with custom task defined three times: Custom tasks get called correctly", async (t) => {
@@ -342,6 +416,133 @@ test.serial("Instantiation with custom task defined three times: Custom tasks ge
 	t.is(customTaskStub.callCount, 3, "Custom task got called three times");
 	["foo", "baz", "bar"].forEach((configuration, index) => {
 		t.deepEqual(customTaskStub.getCall(index).args[0], {
+			options: {
+				projectName: "application.b",
+				projectNamespace: "application/b",
+				configuration
+			},
+			workspace: "myWorkspace",
+			dependencies: "myDependencies"
+		}, `Custom task invocation ${index} got called with expected options`);
+	});
+});
+
+test.serial("Instantiation with custom task defined three times: Custom tasks get called in right order", async (t) => {
+	const customTaskStub = sinon.stub().returns(Promise.resolve());
+	sinon.stub(taskRepository, "getTask").returns({
+		task: customTaskStub,
+		specVersion: "2.0"
+	});
+
+	const project = clone(applicationBTree);
+	project.builder = {
+		customTasks: [{
+			id: "myTaskA",
+			name: "myTask",
+			beforeTask: "myStandardTask",
+			configuration: "foo"
+		}, {
+			id: "myTaskB",
+			name: "myTask",
+			afterTask: "myTaskA",
+			configuration: "bar"
+		}, {
+			id: "myTaskC",
+			name: "myTask",
+			beforeTask: "myTaskA",
+			configuration: "baz"
+		}]
+	};
+
+	const resourceCollections = {
+		workspace: "myWorkspace",
+		dependencies: "myDependencies"
+	};
+	const getInterfaceStub = sinon.stub().returns(undefined);
+	const taskUtil = {
+		getInterface: getInterfaceStub
+	};
+	const customBuilder = new CustomBuilder({project, resourceCollections, taskUtil});
+	await customBuilder.build(["myTask"]);
+
+	t.is(getInterfaceStub.callCount, 3, "taskUtil.getInterface got called three times");
+	t.is(customTaskStub.callCount, 3, "Custom task got called three times");
+	["baz", "foo", "bar"].forEach((configuration, index) => {
+		t.deepEqual(customTaskStub.getCall(index).args[0], {
+			options: {
+				projectName: "application.b",
+				projectNamespace: "application/b",
+				configuration
+			},
+			workspace: "myWorkspace",
+			dependencies: "myDependencies"
+		}, `Custom task invocation ${index} got called with expected options`);
+	});
+});
+
+test.serial("Instantiation with multiple custom task defined: Custom tasks respect tasks to run", async (t) => {
+	const customTaskAStub = sinon.stub().returns(Promise.resolve());
+	const customTaskBStub = sinon.stub().returns(Promise.resolve());
+	sinon.replace(taskRepository, "getTask", sinon.fake((taskName) => ({
+		task: taskName === "myTaskA" ? customTaskAStub : customTaskBStub,
+		specVersion: "2.0"
+	})));
+
+	const project = clone(applicationBTree);
+	project.builder = {
+		customTasks: [{
+			name: "myTaskA",
+			beforeTask: "myStandardTask",
+			configuration: "foo"
+		}, {
+			name: "myTaskB",
+			beforeTask: "myStandardTask",
+			configuration: "baz"
+		}, {
+			name: "myTaskA",
+			afterTask: "myTaskA",
+			configuration: "bar"
+		}, {
+			name: "myTaskB",
+			beforeTask: "myTaskB",
+			configuration: "qux"
+		}]
+	};
+
+	const resourceCollections = {
+		workspace: "myWorkspace",
+		dependencies: "myDependencies"
+	};
+	const getInterfaceStub = sinon.stub().returns(undefined);
+	const taskUtil = {
+		getInterface: getInterfaceStub
+	};
+	const customBuilder1 = new CustomBuilder({project, resourceCollections, taskUtil});
+	await customBuilder1.build(["myTaskA"]);
+
+	t.is(getInterfaceStub.callCount, 2, "taskUtil.getInterface got called two times");
+	t.is(customTaskAStub.callCount, 2, "Custom task A got called two times");
+	t.is(customTaskBStub.callCount, 0, "Custom task B got called zero times");
+	["foo", "bar"].forEach((configuration, index) => {
+		t.deepEqual(customTaskAStub.getCall(index).args[0], {
+			options: {
+				projectName: "application.b",
+				projectNamespace: "application/b",
+				configuration
+			},
+			workspace: "myWorkspace",
+			dependencies: "myDependencies"
+		}, `Custom task invocation ${index} got called with expected options`);
+	});
+
+	const customBuilder2 = new CustomBuilder({project, resourceCollections, taskUtil});
+	await customBuilder2.build(["myTaskB"]);
+
+	t.is(getInterfaceStub.callCount, 4, "taskUtil.getInterface got called two more times");
+	t.is(customTaskAStub.callCount, 2, "Custom task A didn't get called my more times");
+	t.is(customTaskBStub.callCount, 2, "Custom task B got called two times");
+	["qux", "baz"].forEach((configuration, index) => {
+		t.deepEqual(customTaskBStub.getCall(index).args[0], {
 			options: {
 				projectName: "application.b",
 				projectNamespace: "application/b",
@@ -506,6 +707,7 @@ test("addTask: Add task", (t) => {
 	const myFunction = function() {};
 	customBuilder.addTask("myTask", myFunction);
 	t.is(customBuilder.tasks["myTask"], myFunction, "Task has been added to task array");
+	t.true(customBuilder.taskExecutions.myTask.includes("myTask"), "Task executions contains task");
 	t.deepEqual(customBuilder.taskExecutionOrder[customBuilder.taskExecutionOrder.length - 1], "myTask",
 		"Task has been added to end of execution order array");
 });
@@ -520,6 +722,22 @@ test("addTask: Add duplicate task", (t) => {
 	});
 	t.deepEqual(error.message, "Failed to add duplicate task myTask for project application.b",
 		"Correct exception thrown");
+});
+
+test("addTask: Add task with identifier", (t) => {
+	const project = clone(applicationBTree);
+	const customBuilder = new CustomBuilder({project});
+	const myFunction = function() {};
+	customBuilder.addTask("myTaskA", "myTask", myFunction);
+	customBuilder.addTask("myTaskB", "myTask", myFunction);
+	t.is(customBuilder.tasks["myTaskA"], myFunction, "Task A has been added to task array");
+	t.is(customBuilder.tasks["myTaskB"], myFunction, "Task B has been added to task array");
+	t.true(customBuilder.taskExecutions.myTask.includes("myTaskA"), "Task executions contains task A");
+	t.true(customBuilder.taskExecutions.myTask.includes("myTaskB"), "Task executions contains task B");
+	t.deepEqual(customBuilder.taskExecutionOrder[customBuilder.taskExecutionOrder.length - 2], "myTaskA",
+		"Task A has been added to end of execution order array");
+	t.deepEqual(customBuilder.taskExecutionOrder[customBuilder.taskExecutionOrder.length - 1], "myTaskB",
+		"Task B has been added to end of execution order array");
 });
 
 test("addTask: Add task already added to execution order", (t) => {
