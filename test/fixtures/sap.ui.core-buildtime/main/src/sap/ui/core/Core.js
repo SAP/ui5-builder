16796daed1ef66154f4fcc4605d8792ef98b37c3
Lars Kissel <lars.kissel@sap.com>
2021-09-29 12:20:53 +0200

    [INTERNAL] Apply suggestions from code review

--- test/fixtures/sap.ui.core-buildtime/main/src/sap/ui/core/Core.js
+++ test/fixtures/sap.ui.core-buildtime/main/src/sap/ui/core/Core.js
@@ -1,5 +1,5 @@
 /*!
  * ${copyright}
  */
-// replacement of $\{buildtime\} is only made in sap/ui/Global.js
+// replacement of $\{buildtime\} is only made in sap/ui/Global.js and Global-dbg.js
 console.log('${buildtime}');
