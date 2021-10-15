6bb7be11ca6eba63f0b6457457526007529a6e3d
Lars Kissel <lars.kissel@sap.com>
2021-10-15 14:31:25 +0200

    [INTERNAL] Add integration tests

---
+++ test/fixtures/library.l/main/src/library/l/some.js
@@ -0,0 +1,16 @@
+/*!
+ * ${copyright}
+ */
+
+sap.ui.define([],
+	function() {
+	"use strict";
+
+	/**
+	 * @alias library.l
+	 * @namespace
+	 * @public
+	 */
+	var SomeFunction = function() {};
+
+}, /* bExport= */ true);
