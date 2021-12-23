ca7423f9f9081b9128fe08ac1874d08c047803dc
Merlin Beutlberger <m.beutlberger@sap.com>
2021-12-23 11:39:11 +0100

    [INTERNAL] generateStandaloneAppBundle: Update integration test
    Without building all dependencies, some modules might not be minified now

--- test/expected/build/application.b/standalone/resources/sap-ui-custom.js
+++ test/expected/build/application.b/standalone/resources/sap-ui-custom.js
@@ -1,7 +1,10 @@
 //@ui5-bundle sap-ui-custom.js
-jQuery.sap.registerPreloadedModules({
-"version":"2.0",
-"modules":{
+//@ui5-bundle-raw-include ui5loader-autoconfig.js
+(function () {
+	var thisIsTheUi5LoaderAutoconfig = true;
+	console.log(thisIsTheUi5LoaderAutoconfig);
+})()
+sap.ui.require.preload({
 	"id1/embedded/i18n/i18n.properties":'title=embedded-i18n',
 	"id1/embedded/i18n/i18n_de.properties":'title=embedded-i18n_de',
 	"id1/embedded/i18n_fr.properties":'title=embedded-i18n_fr-wrong',
@@ -11,9 +14,12 @@ jQuery.sap.registerPreloadedModules({
 	"id1/i18n/i18n_de.properties":'title=app-i18n_de\nfame=Stra\\u00dfe',
 	"id1/i18n/l10n.properties":'title=app-i18n-wrong\nfame=Stra\\u00dfe',
 	"id1/manifest.json":'{"_version":"1.1.0","sap.app":{"_version":"1.1.0","id":"id1","type":"application","applicationVersion":{"version":"1.2.2"},"embeds":["embedded"],"title":"{{title}}"}}',
-	"sap/ui/core/Core.js":function(){
+	"sap/ui/core/Core.js":function(){(function () {
+	var core = true;
+	console.log(core);
+})()
 }
-}});
+});
 sap.ui.requireSync("sap/ui/core/Core");
 // as this module contains the Core, we ensure that the Core has been booted
 sap.ui.getCore().boot && sap.ui.getCore().boot();
