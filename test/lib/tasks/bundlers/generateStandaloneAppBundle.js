34399d818d3d68ee6ab60a4cd0df34e9fdfa4125
Matthias Osswald <mat.osswald@sap.com>
2022-03-08 15:42:23 +0100

    [INTERNAL] Enhance generateStandaloneAppBundle tests

--- test/lib/tasks/bundlers/generateStandaloneAppBundle.js
+++ test/lib/tasks/bundlers/generateStandaloneAppBundle.js
@@ -9,8 +9,19 @@ let generateStandaloneAppBundle = require("../../../../lib/tasks/bundlers/genera
 test.beforeEach((t) => {
 	// Stubbing processors/bundlers/moduleBundler
 	t.context.moduleBundlerStub = sinon.stub();
-	t.context.moduleBundlerStub.resolves(["I am a resource"]);
+	t.context.moduleBundlerStub.resolves([{bundle: "I am a resource", sourceMap: "I am a source map"}]);
 	mock("../../../../lib/processors/bundlers/moduleBundler", t.context.moduleBundlerStub);
+
+	t.context.taskUtil = {
+		getTag: sinon.stub().returns(false),
+		setTag: sinon.stub(),
+		clearTag: sinon.stub(),
+		STANDARD_TAGS: {
+			HasDebugVariant: "<HasDebugVariant>",
+			IsDebugVariant: "<IsDebugVariant>",
+			OmitFromBuildResult: "<OmitFromBuildResult>"
+		}
+	};
 
 	// Re-require tested module
 	generateStandaloneAppBundle = mock.reRequire("../../../../lib/tasks/bundlers/generateStandaloneAppBundle");
@@ -24,7 +35,7 @@ test.afterEach.always((t) => {
 function createDummyResource(id) {
 	return {
 		getPath: function() {
-			return "ponyPath" + id;
+			return "/resources/ponyPath" + id;
 		}
 	};
 }
@@ -48,7 +59,7 @@ test.serial("execute module bundler and write results", async (t) => {
 	};
 	await generateStandaloneAppBundle(params);
 
-	t.deepEqual(t.context.moduleBundlerStub.callCount, 2, "moduleBundler should be called once");
+	t.deepEqual(t.context.moduleBundlerStub.callCount, 2);
 
 	const {resources, options} = t.context.moduleBundlerStub.getCall(0).args[0];
 	t.deepEqual(resources.length, 4, "moduleBundler got supplied with 4 resources");
@@ -94,7 +105,7 @@ test.serial("execute module bundler and write results without namespace", async 
 	};
 	await generateStandaloneAppBundle(params);
 
-	t.deepEqual(t.context.moduleBundlerStub.callCount, 2, "moduleBundler should be called once");
+	t.deepEqual(t.context.moduleBundlerStub.callCount, 2);
 
 	const {resources, options} = t.context.moduleBundlerStub.getCall(0).args[0];
 	t.deepEqual(resources.length, 4, "moduleBundler got supplied with 4 resources");
@@ -140,7 +151,7 @@ test.serial("execute module bundler and write results in evo mode", async (t) =>
 	};
 	await generateStandaloneAppBundle(params);
 
-	t.deepEqual(t.context.moduleBundlerStub.callCount, 2, "moduleBundler should be called once");
+	t.deepEqual(t.context.moduleBundlerStub.callCount, 2);
 
 	const {resources, options} = t.context.moduleBundlerStub.getCall(0).args[0];
 	t.deepEqual(resources.length, 4, "moduleBundler got supplied with 4 resources");
@@ -155,4 +166,137 @@ test.serial("execute module bundler and write results in evo mode", async (t) =>
 		"!some/project/namespace/test/",
 		"sap/ui/core/Core.js"
 	], "Correct filter in second bundle definition section");
+});
+
+test.serial("execute module bundler with taskUtil", async (t) => {
+	const {taskUtil} = t.context;
+
+	const dummyResource1 = createDummyResource("1.js");
+	const dummyResource2 = createDummyResource("2-dbg.js");
+	const dummyResource3 = createDummyResource("3.js");
+	const dummyResource4 = createDummyResource("4-dbg.js");
+
+	taskUtil.getTag.withArgs(dummyResource1, taskUtil.STANDARD_TAGS.HasDebugVariant).returns(true);
+	taskUtil.getTag.withArgs(dummyResource2.getPath(), taskUtil.STANDARD_TAGS.IsDebugVariant).returns(true);
+
+	const ui5LoaderDummyResource = {
+		getPath: function() {
+			return "/resources/ui5loader.js"; // Triggers evo mode
+		}
+	};
+	const dummyReaderWriter = {
+		_byGlob: async function() {
+			return [
+				ui5LoaderDummyResource,
+				dummyResource1,
+				dummyResource2,
+				dummyResource3,
+				dummyResource4,
+			];
+		},
+		write: function() {}
+	};
+	sinon.stub(dummyReaderWriter, "write").resolves();
+	const params = {
+		workspace: dummyReaderWriter,
+		dependencies: dummyReaderWriter,
+		taskUtil,
+		options: {
+			projectName: "some.project.name",
+			namespace: "some/project/namespace"
+		}
+	};
+	await generateStandaloneAppBundle(params);
+
+	t.is(t.context.moduleBundlerStub.callCount, 2);
+
+	t.is(t.context.moduleBundlerStub.getCall(0).args.length, 1);
+	t.deepEqual(t.context.moduleBundlerStub.getCall(0).args[0].options, {
+		bundleDefinition: {
+			defaultFileTypes: [
+				".js",
+				".control.xml",
+				".fragment.html",
+				".fragment.json",
+				".fragment.xml",
+				".view.html",
+				".view.json",
+				".view.xml",
+				".properties",
+			],
+			name: "sap-ui-custom.js",
+			sections: [
+				{
+					declareModules: false,
+					filters: [
+						"ui5loader-autoconfig.js",
+					],
+					mode: "raw",
+					resolve: true,
+					sort: true,
+				},
+				{
+					filters: [
+						"some/project/namespace/",
+						"some/project/namespace/**/manifest.json",
+						"some/project/namespace/changes/changes-bundle.json",
+						"some/project/namespace/changes/flexibility-bundle.json",
+						"!some/project/namespace/test/",
+						"sap/ui/core/Core.js",
+					],
+					mode: "preload",
+					renderer: true,
+					resolve: true,
+					resolveConditional: true,
+				},
+				{
+					filters: [
+						"sap/ui/core/Core.js",
+					],
+					mode: "require",
+				},
+			],
+		}
+	});
+
+	t.is(t.context.moduleBundlerStub.getCall(1).args.length, 1);
+	t.deepEqual(t.context.moduleBundlerStub.getCall(1).args[0].options, {
+		bundleDefinition: {
+			defaultFileTypes: [
+				".js",
+				".control.xml",
+				".fragment.html",
+				".fragment.json",
+				".fragment.xml",
+				".view.html",
+				".view.json",
+				".view.xml",
+				".properties",
+			],
+			name: "sap-ui-custom-dbg.js",
+			sections: [
+				{
+					declareModules: false,
+					filters: [
+						"ui5loader-autoconfig.js",
+					],
+					mode: "raw",
+					resolve: true,
+					sort: true,
+				},
+				{
+					filters: [
+						"sap/ui/core/Core.js",
+					],
+					mode: "require",
+				},
+			],
+		},
+		bundleOptions: {
+			optimize: false
+		},
+		moduleNameMapping: {
+			"/resources/ponyPath2-dbg.js": "ponyPath2.js"
+		}
+	});
 });
