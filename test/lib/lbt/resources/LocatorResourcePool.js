SAP/ui5-builder

commit 8a20c4257a1ebe4d613b0595be93cd45d900f580

[FEATURE] Generate source maps for bundles (#695)
Resolves https://github.com/SAP/ui5-tooling/issues/472
Supersedes https://github.com/SAP/ui5-builder/pull/282
Based on https://github.com/SAP/ui5-tooling/pull/583
JIRA: CPOUI5FOUNDATION-434

Co-authored-by: Matthias Osswald <mat.osswald@sap.com>

lib/lbt/resources/LocatorResourcePool.js
@@ -2,12 +2,16 @@ const ResourcePool = require("./ResourcePool");
 const LocatorResource = require("./LocatorResource");
 
 class LocatorResourcePool extends ResourcePool {
-	prepare(resources) {
+	prepare(resources, moduleNameMapping) {
 		resources = resources.filter( (res) => !res.getStatInfo().isDirectory() );
 		return Promise.all(
-			resources.map(
-				(resource) => this.addResource( new LocatorResource(this, resource) )
-			).filter(Boolean)
+			resources.map((resource) => {
+				let moduleName = moduleNameMapping && moduleNameMapping[resource.getPath()];
+				if (!moduleName) {
+					moduleName = resource.getPath().slice("/resources/".length);
+				}
+				this.addResource(new LocatorResource(this, resource, moduleName));
+			}).filter(Boolean)
 		);
 	}
 }

commit 1b7f93f4988340d7a6575be3191a02e6c295ebd0

[FIX] LocatorResourcePool: Wait for resources in prepare step (#719)
Issue was introduced with #695.

lib/lbt/resources/LocatorResourcePool.js
@@ -10,7 +10,7 @@ class LocatorResourcePool extends ResourcePool {
 				if (!moduleName) {
 					moduleName = resource.getPath().slice("/resources/".length);
 				}
-				this.addResource(new LocatorResource(this, resource, moduleName));
+				return this.addResource(new LocatorResource(this, resource, moduleName));
 			}).filter(Boolean)
 		);
 	}

test/lib/lbt/resources/LocatorResourcePool.js
@@ -1,8 +1,24 @@
 const test = require("ava");
 const LocatorResourcePool = require("../../../../lib/lbt/resources/LocatorResourcePool");
+const Resource = require("@ui5/fs").Resource;
 
 test("getIgnoreMissingModules", async (t) => {
 	const resourcePool = new LocatorResourcePool({ignoreMissingModules: true});
 	t.true(resourcePool.getIgnoreMissingModules(), "ignoreMissingModules is true");
+});
+
+test("wait for resources to finish prepare", async (t) => {
+	let promiseResolved = false;
+	const promise = new Promise((resolve) => {
+		setTimeout(() => {
+			promiseResolved = true;
+			resolve();
+		}, 10);
+	});
+	const resourcePool = new LocatorResourcePool();
+	resourcePool.addResource = () => promise;
+	return resourcePool.prepare([new Resource({path: "mypath"})]).then(() => {
+		t.true(promiseResolved, "addResources promise is resolved before prepare promise is resolved");
+	});
 });
 
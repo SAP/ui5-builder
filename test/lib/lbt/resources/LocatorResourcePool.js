1b7f93f4988340d7a6575be3191a02e6c295ebd0
Florian Vogt <florian.vogt@sap.com>
2022-03-09 10:40:18 +0100

    [FIX] LocatorResourcePool: Wait for resources in prepare step (#719)
    Issue was introduced with #695.

--- test/lib/lbt/resources/LocatorResourcePool.js
+++ test/lib/lbt/resources/LocatorResourcePool.js
@@ -1,8 +1,24 @@
 const test = require("ava");
 const LocatorResourcePool = require("../../../../lib/lbt/resources/LocatorResourcePool");
+const Resource = require("@ui5/fs").Resource;
 
 test("getIgnoreMissingModules", async (t) => {
 	const resourcePool = new LocatorResourcePool({ignoreMissingModules: true});
 	t.true(resourcePool.getIgnoreMissingModules(), "ignoreMissingModules is true");
+});
+
+test("wait for resources to finish prepare", async (t) => {
+	let promiseResolved = false;
+	const promise = new Promise((resolve) => {
+		setTimeout(() => {
+			promiseResolved = true;
+			resolve();
+		}, 10);
+	});
+	const resourcePool = new LocatorResourcePool();
+	resourcePool.addResource = () => promise;
+	return resourcePool.prepare([new Resource({path: "mypath"})]).then(() => {
+		t.true(promiseResolved, "addResources promise is resolved before prepare promise is resolved");
+	});
 });
 
