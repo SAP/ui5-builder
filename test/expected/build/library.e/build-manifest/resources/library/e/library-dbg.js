fb0f8978fdf9d56835d6122c720f4c259649df0f
Merlin Beutlberger <m.beutlberger@sap.com>
2022-07-27 17:23:44 +0200

    [INTERNAL] builder: Add integration tests for build-manifest builds

---
+++ test/expected/build/library.e/build-manifest/resources/library/e/library-dbg.js
@@ -0,0 +1,8 @@
+/*!
+ * UI development toolkit for HTML5 (OpenUI5)
+ * (c) Copyright 2009-xxx SAP SE or an SAP affiliate company.
+ * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
+ */
+console.log('HelloWorld');
+
+// 1.0.0
---
+++ test/expected/build/library.i/bundle-library.h-build-manifest/resources/library/i/library-dbg.js
@@ -0,0 +1,38 @@
+/*!
+ * Some fancy copyright
+ */
+sap.ui.define([
+	'sap/ui/core/Core',
+], function(Core) {
+
+	"use strict";
+
+	sap.ui.getCore().initLibrary({
+		name : "library.i",
+		version: "1.0.0",
+		dependencies : ["sap.ui.core"],
+		types: [
+			"library.i.ButtonType",
+			"library.i.DialogType",
+		],
+		interfaces: [
+			"library.i.IContent",
+		],
+		controls: [
+			"library.i.Button",
+			"library.i.CheckBox",
+			"library.i.Dialog",
+			"library.i.Input",
+			"library.i.Label",
+			"library.i.Link",
+			"library.i.Menu",
+			"library.i.Text"
+		],
+		elements: [
+			"library.i.MenuItem"
+		],
+	});
+
+	return thisLib;
+
+});
