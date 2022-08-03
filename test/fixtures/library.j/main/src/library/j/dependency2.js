a1cce383e1490df80bf1a87d3a15967f25d35e4d
Yavor Ivanov <yavor.ivanov@sap.com>
2022-08-03 15:32:13 +0300

    Modify fixtres with destructuring cases

---
+++ test/fixtures/library.j/main/src/library/j/dependency2.js
@@ -0,0 +1,31 @@
+sap.ui.define([], function () {
+	sap.ui.getCore().initLibrary({
+		name: "testlib",
+		version: "${version}",
+		dependencies: ["sap.ui.core"],
+		designtime: "testlib/designtime/library.designtime",
+		types: ["testlib.AnotherValidEnum"],
+	});
+
+	/**
+	 * AnotherValidEnum
+	 *
+	 * @enum {string}
+	 * @public
+	 * @ui5-metamodel This enumeration also will be described in the UI5 (legacy) designtime metamodel
+	 */
+	testlib.AnotherValidEnum = {
+		/**
+		 * Fizz
+		 * @public
+		 */
+		Fizz: "Fizz",
+		/**
+		 * Bar
+		 * @public
+		 */
+		Buzz: "Buzz",
+	};
+
+	return testlib;
+});
