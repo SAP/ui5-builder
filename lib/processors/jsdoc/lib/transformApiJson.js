SAP/ui5-builder

commit d48d2ba171f4da0bbed26473e3e7b94fde59bca9

Update JSDoc template with current openui5/master
Consists of the following changes

  * [INTERNAL] transformApiJson: Fix reading docuindex.json
     Cherry picked from SAP/openui5@f883bf8e9
  * [INTERNAL] transformApiJson: Fix path handling on Windows
     Cherry picked from SAP/openui5@dacd7fc11

lib/processors/jsdoc/lib/transformApiJson.js
@@ -25,6 +25,11 @@ const log = (function() {
 	}
 }());
 
+function replaceLastPathSegment(p, replacement) {
+	// Note: path.join also correctly normalizes any POSIX paths on Windows
+	return path.join(path.dirname(p), replacement);
+}
+
 /*
  * Transforms the api.json as created by the JSDoc build into a pre-processed api.json file suitable for the SDK.
  *
@@ -981,6 +986,57 @@ function transformer(sInputFile, sOutputFile, sLibraryFile, vDependencyAPIFiles,
 			// Normalize path to resolve relative path
 			sPath = path.normalize(sPath);
 
+			const {sources} = options;
+			let {librarySrcDir, libraryTestDir} = options;
+
+			// Using path.join to normalize POSIX paths to Windows paths on Windows
+			librarySrcDir = path.join(librarySrcDir);
+			libraryTestDir = path.join(libraryTestDir);
+
+			if (Array.isArray(sources) && typeof librarySrcDir === "string" && typeof libraryTestDir === "string") {
+				/**
+				 * Calculate prefix to check
+				 *
+				 * Example 1:
+				 * - sSource: /path/to/project/src
+				 * - sLibrarySrcDir: src
+				 *
+				 * Prefix: /path/to/project/test-resources
+				 *
+				 * Example 2:
+				 * - sSource: /path/to/project/src/main/js
+				 * - sLibrarySrcDir: src/main/js
+				 *
+				 * Prefix: /path/to/project/src/main/test-resources
+				 */
+				const librarySrcDirWithTestResources = replaceLastPathSegment(librarySrcDir, "test-resources");
+
+				for (const sourcePath of sources) {
+					/**
+					 * Replace prefix with file system path
+					 *
+					 * Example 1:
+					 * - sPath: /path/to/project/test-resources/sap/test/demokit/docuindex.json
+					 *
+					 * New sPath: /path/to/project/test/sap/test/demokit/docuindex.json
+					 *
+					 * Example 2:
+					 * - sPath: /path/to/project/src/main/test-resources/sap/test/demokit/docuindex.json
+					 *
+					 * New sPath: /path/to/project/src/main/test/sap/test/demokit/docuindex.json
+					 */
+					if (!sourcePath.endsWith(librarySrcDir)) {
+						continue;
+					}
+					const libraryDir = sourcePath.substring(0, sourcePath.lastIndexOf(librarySrcDir) - 1);
+					const prefix = path.join(libraryDir, librarySrcDirWithTestResources);
+					if (sPath.startsWith(prefix)) {
+						sPath = path.join(libraryDir, libraryTestDir, sPath.substring(prefix.length));
+						break;
+					}
+				}
+			}
+
 			fs.readFile(sPath, 'utf8', (oError, oFileData) => {
 				if (!oError) {
 					oFileData = JSON.parse(oFileData);
@@ -991,7 +1047,7 @@ function transformer(sInputFile, sOutputFile, sLibraryFile, vDependencyAPIFiles,
 						});
 					}
 				}
-				// We aways resolve as this data is not mandatory
+				// We always resolve as this data is not mandatory
 				oResolve(oChainObject);
 			});
 

commit bb304bd438eb51865aad19af655fd5d005e63507

[FIX] transformApiJson: Fix regression in UI5 Tooling scenario
Caused by https://github.com/SAP/ui5-builder/pull/793/commits/d48d2ba171f4da0bbed26473e3e7b94fde59bca9

(cherry picked from https://github.com/SAP/openui5/commit/d3bcca92ff86fb3c5d4b2610ada50f7d85af2a51)
(cherry picked from commit 7d0ac119078d4e04afb646df3b9ee16dfff511b2)

lib/processors/jsdoc/lib/transformApiJson.js
@@ -989,11 +989,12 @@ function transformer(sInputFile, sOutputFile, sLibraryFile, vDependencyAPIFiles,
 			const {sources} = options;
 			let {librarySrcDir, libraryTestDir} = options;
 
-			// Using path.join to normalize POSIX paths to Windows paths on Windows
-			librarySrcDir = path.join(librarySrcDir);
-			libraryTestDir = path.join(libraryTestDir);
-
 			if (Array.isArray(sources) && typeof librarySrcDir === "string" && typeof libraryTestDir === "string") {
+
+				// Using path.join to normalize POSIX paths to Windows paths on Windows
+				librarySrcDir = path.join(librarySrcDir);
+				libraryTestDir = path.join(libraryTestDir);
+
 				/**
 				 * Calculate prefix to check
 				 *