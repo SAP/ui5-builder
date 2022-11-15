ec7cee0356af0a946ae9b0caf4f56f80fde83e1e
Yavor Ivanov <yavor.ivanov@sap.com>
2022-11-15 09:10:20 +0200

    Define new scenarios for destructuring
    - Nested ObjectPattern destructuring
    - ArrayPattern handling
    - ArrayPattern + (nested) ObjectPattern

--- test/expected/build/library.j/dest/resources/library/j/ValidPropertyDefaultValue.js
+++ test/expected/build/library.j/dest/resources/library/j/ValidPropertyDefaultValue.js
@@ -1,104 +1,163 @@
 /*!
  * ${copyright}
  */
-sap.ui.define([
-	"sap/ui/core/Control",
-	"./library",
-	"./core/library"
-], (Control, { MyValidEnum, ThisIsEnumToo: RenamedEnum }, coreLibrary) => {
-	const { AnotherValidEnum } = coreLibrary;
-	const { Buzz } = AnotherValidEnum;
-	const { AnotherValidEnum: AnotherRenamedEnum } = coreLibrary;
-	const { H1 } = sap.ui.core.TitleLevel;
-	const { Value2: RenamedValue2 } = RenamedEnum;
+sap.ui.define(
+	[
+		"sap/ui/core/Control",
+		"./library",
+		"./core/library",
+		"sap/external/thirdparty/library",
+		"sap/external2/thirdparty/library",
+	],
+	(
+		Control,
+		{ MyValidEnum, ThisIsEnumToo: RenamedEnum },
+		coreLibrary,
+		[arrPattern, {arrWith: {deep: arrPatternDeepDestruct}}],
+		{ objPattern: {deeply: {destructured: objPatternDeepDestruct}, objPattern1Lvl} }
+	) => {
+		const { AnotherValidEnum } = coreLibrary;
+		const { Buzz } = AnotherValidEnum;
+		const { AnotherValidEnum: {Buzz: BuzzRenamed} } = coreLibrary;
+		const { AnotherValidEnum: AnotherRenamedEnum } = coreLibrary;
+		const { H1 } = sap.ui.core.TitleLevel;
+		const { Value2: RenamedValue2 } = RenamedEnum;
 
-	/**
-	 * @class
-	 * My super documentation of this class
-	 *
-	 * @extends sap.ui.core.Control
-	 *
-	 * @author SAP SE
-	 * @version ${version}
-	 *
-	 * @public
-	 * @alias library.j.ValidPropertyDefaultValue
-	 * @ui5-metamodel text
-	 */
-	var ValidPropertyDefaultValue = Control.extend(
-		"library.j.ValidPropertyDefaultValue",
-		{
-			metadata: {
-				properties: {
-					/**
-					 * validPropertyDefaultValueEnumSimpleDestructuring
-					 */
-					validPropertyDefaultValueEnumSimpleDestructuring: {
-						type: "library.j.core.AnotherValidEnum",
-						group: "Misc",
-						defaultValue: AnotherValidEnum.Buzz,
-					},
-
-					/**
-					 * validPropertyDefaultValueEnumChainedDestructuring
-					 */
-					validPropertyDefaultValueEnumChainedDestructuring: {
-						type: "library.j.core.AnotherValidEnum",
-						group: "Misc",
-						defaultValue: Buzz,
-					},
-
-					/**
-					 * validPropertyDefaultValueEnumDestructuringWithRename
-					 */
-					validPropertyDefaultValueEnumDestructuringWithRename: {
-						type: "library.j.core.AnotherValidEnum",
-						group: "Misc",
-						defaultValue: AnotherRenamedEnum.Fizz,
-					},
-
-					/**
-					 * validPropertyDefaultValueEnumDestructuringWithRenameInArguments
-					 */
-					validPropertyDefaultValueEnumDestructuringWithRenameInArguments:
-						{
-							type: "library.j.ThisIsEnumToo",
+		/**
+		 * @class
+		 * My super documentation of this class
+		 *
+		 * @extends sap.ui.core.Control
+		 *
+		 * @author SAP SE
+		 * @version ${version}
+		 *
+		 * @public
+		 * @alias library.j.ValidPropertyDefaultValue
+		 * @ui5-metamodel text
+		 */
+		var ValidPropertyDefaultValue = Control.extend(
+			"library.j.ValidPropertyDefaultValue",
+			{
+				metadata: {
+					properties: {
+						/**
+						 * validPropertyDefaultValueEnumSimpleDestructuring
+						 */
+						validPropertyDefaultValueEnumSimpleDestructuring: {
+							type: "library.j.core.AnotherValidEnum",
 							group: "Misc",
-							defaultValue: RenamedEnum.Value1,
+							defaultValue: AnotherValidEnum.Buzz,
 						},
 
-					/**
-					 * validPropertyDefaultValueEnumDestructuringWithRenameInArgumentsAndLocalVar
-					 */
-					validPropertyDefaultValueEnumDestructuringWithRenameInArgumentsAndLocalVar:
-						{
-							type: "library.j.ThisIsEnumToo",
+						/**
+						 * validPropertyDefaultValueEnumChainedDestructuring
+						 */
+						 validPropertyDefaultValueEnumChainedDestructuring: {
+							type: "library.j.core.AnotherValidEnum",
 							group: "Misc",
-							defaultValue: RenamedValue2,
+							defaultValue: Buzz,
 						},
 
-					/**
-					 * validPropertyDefaultValueEnumViaDestructuringInArrowFn
-					 */
-					validPropertyDefaultValueEnumViaDestructuringInArrowFn: {
-						type: "library.j.MyValidEnum",
-						group: "Misc",
-						defaultValue: MyValidEnum.Foo,
-					},
+						/**
+						 * validPropertyDefaultValueEnumNestedDestructuring
+						 */
+						 validPropertyDefaultValueEnumChainedDestructuring: {
+							type: "library.j.core.AnotherValidEnum",
+							group: "Misc",
+							defaultValue: BuzzRenamed,
+						},
 
-					/**
-					 * validPropertyDefaultValueEnumViaDestructuringGlobal
-					 */
-					validPropertyDefaultValueEnumViaDestructuringGlobal: {
-						type: "sap.ui.core.TitleLevel",
-						group: "Misc",
-						defaultValue: H1,
+						/**
+						 * validPropertyDefaultValueEnumDestructuringWithRename
+						 */
+						validPropertyDefaultValueEnumDestructuringWithRename: {
+							type: "library.j.core.AnotherValidEnum",
+							group: "Misc",
+							defaultValue: AnotherRenamedEnum.Fizz,
+						},
+
+						/**
+						 * validPropertyDefaultValueEnumDestructuringWithRenameInArguments
+						 */
+						validPropertyDefaultValueEnumDestructuringWithRenameInArguments:
+							{
+								type: "library.j.ThisIsEnumToo",
+								group: "Misc",
+								defaultValue: RenamedEnum.Value1,
+							},
+
+						/**
+						 * validPropertyDefaultValueEnumDestructuringWithRenameInArgumentsAndLocalVar
+						 */
+						validPropertyDefaultValueEnumDestructuringWithRenameInArgumentsAndLocalVar:
+							{
+								type: "library.j.ThisIsEnumToo",
+								group: "Misc",
+								defaultValue: RenamedValue2,
+							},
+
+						/**
+						 * validPropertyDefaultValueEnumViaDestructuringInArrowFn
+						 */
+						validPropertyDefaultValueEnumViaDestructuringInArrowFn:
+							{
+								type: "library.j.MyValidEnum",
+								group: "Misc",
+								defaultValue: MyValidEnum.Foo,
+							},
+
+						/**
+						 * validPropertyDefaultValueEnumViaDestructuringGlobal
+						 */
+						 validPropertyDefaultValueEnumViaDestructuringGlobal: {
+							type: "sap.ui.core.TitleLevel",
+							group: "Misc",
+							defaultValue: H1,
+						},
+
+						/**
+						 * validPropertyDefaultValueArrPattern
+						 */
+						 validPropertyDefaultValueArrPattern: {
+							type: "sap.external.thirdparty.library",
+							group: "Misc",
+							defaultValue: arrPattern,
+						},
+
+						/**
+						 * validPropertyDefaultValueArrPatternDeepDestruct
+						 */
+						 validPropertyDefaultValueArrPatternDeepDestruct: {
+							type: "sap.external.thirdparty.library.arrWith.deep",
+							group: "Misc",
+							defaultValue: arrPatternDeepDestruct,
+						},
+
+						/**
+						 * validPropertyDefaultValueArrPatternDeepDestruct
+						 */
+						 validPropertyDefaultValueObjPatternDeepDestruct: {
+							type: "sap.external2.thirdparty.library.objPattern.deeply",
+							group: "Misc",
+							defaultValue: objPatternDeepDestruct,
+						},
+
+						/**
+						 * validPropertyDefaultValueObjPatternNested
+						 */
+						 validPropertyDefaultValueObjPatternNested: {
+							type: "sap.external2.thirdparty.library.objPattern",
+							group: "Misc",
+							defaultValue: objPattern1Lvl,
+						}
 					},
 				},
-			},
-			renderer: function () {},
-		}
-	);
+				renderer: function () {},
+			}
+		);
 
-	return ValidPropertyDefaultValue;
-}, /* bExport= */ true);
+		return ValidPropertyDefaultValue;
+	},
+	/* bExport= */ true
+);
