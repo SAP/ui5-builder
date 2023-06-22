916b4f16003afe73813a15b4da4e8dd7ad302a54
Yavor Ivanov <yavor.ivanov@sap.com>
2023-06-22 13:22:55 +0300

    Fix tests so far...

--- test/lib/lbt/resources/ResourceCollector.js
+++ test/lib/lbt/resources/ResourceCollector.js
@@ -434,8 +434,7 @@ test.serial("integration: Analyze debug bundle", async (t) => {
 	t.is(myRawModuleBundle.requiresTopLevelScope, false);
 	t.deepEqual(myRawModuleBundle.included,
 		new Set(["a.js", "b.js"]));
-	t.deepEqual(myRawModuleBundle.required,
-		new Set([]));
+	t.is(myRawModuleBundle.required, null);
 
 	const myRawModuleBundleDbg = resourceInfoList.resourcesByName.get("myBundle-dbg.js");
 	t.is(myRawModuleBundleDbg.name, "myBundle-dbg.js");
@@ -444,6 +443,5 @@ test.serial("integration: Analyze debug bundle", async (t) => {
 	t.is(myRawModuleBundleDbg.requiresTopLevelScope, false);
 	t.deepEqual(myRawModuleBundleDbg.included,
 		new Set(["a.js"]));
-	t.deepEqual(myRawModuleBundleDbg.required,
-		new Set());
+	t.is(myRawModuleBundleDbg.required, null);
 });
