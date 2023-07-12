8f23f388f6d64f313ae8a89d0fcaf39ba905a70b
Frank Weigel <frank.weigel@sap.com>
2023-07-12 21:24:59 +0200

    [FIX] Don't report a missing module that's contained in another bundle (#915)

--- test/lib/lbt/bundle/Resolver.js
+++ test/lib/lbt/bundle/Resolver.js
@@ -1,4 +1,7 @@
 import test from "ava";
+import sinon from "sinon";
+import esmock from "esmock";
+
 import Resolver from "../../../../lib/lbt/bundle/Resolver.js";
 import ResourcePool from "../../../../lib/lbt/resources/ResourcePool.js";
 
@@ -228,4 +231,73 @@ test.serial("embedd a non-decomposable bundle", async (t) => {
 		[
 			"vendor/non-decomposable-bundle.js"
 		], "new bundle should contain the non-decomposable bundle");
+});
+
+test.serial("no errors for dependencies between non-decomposable bundles", async (t) => {
+	const errorLogStub = sinon.stub();
+	const myLoggerInstance = {
+		error: errorLogStub,
+		silly: sinon.stub(),
+		verbose: sinon.stub()
+	};
+	const ResolverWithStub = await esmock("../../../../lib/lbt/bundle/Resolver", {
+		"@ui5/logger": {
+			getLogger: () => myLoggerInstance
+		}
+	});
+
+	const pool = new MockPool({
+		"lib/mod1.js": TRIVIAL_MODULE,
+		"lib/mod2.js": TRIVIAL_MODULE,
+		"lib/mod3.js": "sap.ui.define(['lib/mod4'], function() {});",
+		"lib/mod4.js": TRIVIAL_MODULE,
+		"vendor/non-decomposable-bundle1.js": `
+			define("external1/mod1", function() {});
+			define("external1/mod2", function() {});
+			define("external1/mod3", function() {});
+			define("external1/mod4", function() {});`,
+		"vendor/non-decomposable-bundle2.js": `
+			define("external2/mod1", ["lib/mod1"], function() {}); // exists in pool 
+			define("external2/mod2", ["external1/mod1"], function() {}); // exists in previous bundle
+			define("external2/mod3", ["external2/mod2"], function() {}); // exists in this bundle
+			define("external2/mod4", ["external3/mod3"], function() {}); // exists in next bundle
+			define("external2/mod5", ["external4/mod1"], function() {}); // missing`,
+		"vendor/non-decomposable-bundle3.js": `
+			define("external3/mod1", function() {});
+			define("external3/mod2", function() {});
+			define("external3/mod3", function() {});`
+	});
+
+	const bundleDefinition = {
+		name: "bundle.js",
+		sections: [
+			{
+				mode: "preload",
+				filters: [
+					"vendor/"
+				],
+				resolve: true
+			}
+		]
+	};
+
+	const resolver = new ResolverWithStub(pool);
+
+	const resolvedBundle = await resolver.resolve(bundleDefinition);
+
+	t.true(resolvedBundle != null, "resolve() should return a bundle");
+	t.is(resolvedBundle.sections.length, 1, "bundle should contain 1 section");
+	t.deepEqual(
+		sortedCopy(resolvedBundle.sections[0].modules),
+		[
+			"lib/mod1.js",
+			"vendor/non-decomposable-bundle1.js",
+			"vendor/non-decomposable-bundle2.js",
+			"vendor/non-decomposable-bundle3.js"
+		], "new bundle should contain the non-decomposable bundle");
+
+	t.is(errorLogStub.callCount, 1, "One error reported");
+	t.is(errorLogStub.firstCall.args[0],
+		"**** error: missing module external4/mod1.js, required by vendor/non-decomposable-bundle2.js",
+		"only the expected missing module is reported");
 });
