f7c05db57cd976012f83ec696ec15e5828eb8386
Matthias Osswald <mat.osswald@sap.com>
2023-09-29 15:16:06 +0200

    [INTERNAL] Add builder source maps test
    Currently only fails because default of "useInputSourceMaps" option
    of minify task is set to "false".

---
+++ test/fixtures/sourcemaps/test.application/webapp/TypeScriptSource.js
@@ -0,0 +1,2 @@
+"use strict";sap.ui.define([],function(){"use strict";var i={functionWithinTypeScriptSource(){functionCallWithinTypeScriptSource()}};return i});
+//# sourceMappingURL=TypeScriptSource.js.map
