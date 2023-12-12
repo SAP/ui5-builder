ff75dadd2aa049413b66972a2f769895fefac505
Florian Vogt <florian.vogt@sap.com>
2023-12-12 15:45:01 +0100

    add unit test with terminilogies for processor

--- test/lib/processors/manifestTransformer.js
+++ test/lib/processors/manifestTransformer.js
@@ -826,3 +826,174 @@ test.serial("Library: sap.ui5/library: Do not replace supportedLocales with disa
 	t.true(t.context.logWarnSpy.notCalled, "No warnings should be logged");
 	t.true(t.context.logErrorSpy.notCalled, "No errors should be logged");
 });
+
+test.serial("Library: sap.ui5/library: Replaces supportedLocales with terminologies", async (t) => {
+	t.plan(4);
+	const {manifestTransformer} = t.context;
+	const input = JSON.stringify({
+		"_version": "1.58.0",
+		"sap.app": {
+			"id": "sap.ui.demo.lib",
+			"type": "library"
+		},
+		"sap.ui5": {
+			"library": {
+				"i18n": {
+					"bundleUrl": "i18nc/messagebundlec.properties",
+					"terminologies": {
+						"sports": {
+							"bundleUrl": "i18nc_sports/messagebundle.sports.properties"
+						}
+					}
+				}
+			}
+		}
+	}, null, 2);
+
+	const expected = JSON.stringify({
+		"_version": "1.58.0",
+		"sap.app": {
+			"id": "sap.ui.demo.lib",
+			"type": "library"
+		},
+		"sap.ui5": {
+			"library": {
+				"i18n": {
+					"bundleUrl": "i18nc/messagebundlec.properties",
+					"terminologies": {
+						"sports": {
+							"bundleUrl": "i18nc_sports/messagebundle.sports.properties",
+							"supportedLocales": ["", "de", "en"]
+						}
+					},
+					"supportedLocales": ["", "de", "en"],
+				}
+			}
+		}
+	}, null, 2);
+
+	const resource = {
+		getString: () => Promise.resolve(input),
+		setString: (actual) => {
+			t.deepEqual(actual, expected, "Correct file content should be set");
+		}
+	};
+
+	const processedResources = await manifestTransformer({
+		resources: [resource],
+		fs: {
+			readdir: sinon.stub().callsFake((fsPath, callback) => {
+				if (fsPath & fsPath.startsWith("i18nc_sports")) {
+					return callback(null, [
+						"messagebundle.sports_de.properties",
+						"messagebundle.sports_en.properties",
+						"messagebundle.sports.properties"
+					]);
+				} else {
+					return callback(null, [
+						"messagebundlec_de.properties",
+						"messagebundlec_en.properties",
+						"messagebundlec.properties"
+					]);
+				}
+			})
+		}
+	});
+
+	t.deepEqual(processedResources, [resource], "Input resource is returned");
+
+	t.true(t.context.logWarnSpy.notCalled, "No warnings should be logged");
+	t.true(t.context.logErrorSpy.notCalled, "No errors should be logged");
+});
+
+test.serial("Library: sap.ui5/library: Replaces supportedLocales with enhanceWith", async (t) => {
+	t.plan(4);
+	const {manifestTransformer} = t.context;
+	const input = JSON.stringify({
+		"_version": "1.58.0",
+		"sap.app": {
+			"id": "sap.ui.demo.lib",
+			"type": "library"
+		},
+		"sap.ui5": {
+			"library": {
+				"i18n": {
+					"bundleUrl": "i18nc/messagebundlec.properties",
+					"enhanceWith": [
+						{
+							"bundleUrl": "myfolder1/messagebundlenc1.properties"
+						},
+						{
+							"bundleUrl": "myfolder2/messagebundlenc2.properties"
+						}
+					]
+				}
+			}
+		}
+	}, null, 2);
+
+	const expected = JSON.stringify({
+		"_version": "1.58.0",
+		"sap.app": {
+			"id": "sap.ui.demo.lib",
+			"type": "library"
+		},
+		"sap.ui5": {
+			"library": {
+				"i18n": {
+					"bundleUrl": "i18nc/messagebundlec.properties",
+					"enhanceWith": [
+						{
+							"bundleUrl": "myfolder1/messagebundlenc1.properties",
+							"supportedLocales": ["", "de", "en"]
+						},
+						{
+							"bundleUrl": "myfolder1/messagebundlenc1.properties",
+							"supportedLocales": ["", "de", "en"]
+						}
+					],
+					"supportedLocales": ["", "de", "en"],
+				}
+			}
+		}
+	}, null, 2);
+
+	const resource = {
+		getString: () => Promise.resolve(input),
+		setString: (actual) => {
+			t.deepEqual(actual, expected, "Correct file content should be set");
+		}
+	};
+
+	const processedResources = await manifestTransformer({
+		resources: [resource],
+		fs: {
+			readdir: sinon.stub().callsFake((fsPath, callback) => {
+				if (fsPath & fsPath.startsWith("myfolder1")) {
+					return callback(null, [
+						"messagebundlenc1_de.properties",
+						"messagebundlenc1_en.properties",
+						"messagebundlenc1.properties"
+					]);
+				} else if (fsPath & fsPath.startsWith("myfolder2")) {
+					return callback(null, [
+						"messagebundlenc2_de.properties",
+						"messagebundlenc2_en.properties",
+						"messagebundlenc2.properties"
+					]);
+				} else {
+					return callback(null, [
+						"messagebundlec_de.properties",
+						"messagebundlec_en.properties",
+						"messagebundlec.properties"
+					]);
+				}
+			})
+		}
+	});
+
+	t.deepEqual(processedResources, [resource], "Input resource is returned");
+
+	t.true(t.context.logWarnSpy.notCalled, "No warnings should be logged");
+	t.true(t.context.logErrorSpy.notCalled, "No errors should be logged");
+});
