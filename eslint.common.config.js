e0489459c2952a890f05048e0e162c34968e84fb
Max Reichmann <max.reichmann@sap.com>
2024-08-02 15:07:17 +0200

    [INTERNAL] Remove unused var ESLint rule

--- eslint.common.config.js
+++ eslint.common.config.js
@@ -67,13 +67,6 @@ export default [{
 		"no-tabs": "off",
 		"no-console": 2, // Disallow console.log()
 		"no-eval": 2,
-		"no-unused-vars": [
-			"error", {
-				argsIgnorePattern: "^_",
-				varsIgnorePattern: "^_",
-				caughtErrorsIgnorePattern: "^_",
-			},
-		],
 		// The following rule must be disabled as of ESLint 9.
 		// It's removed and causes issues when present
 		// https://eslint.org/docs/latest/rules/valid-jsdoc
