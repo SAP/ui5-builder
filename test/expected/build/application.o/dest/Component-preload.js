a6c04d26ae964566c82e82d1be2ef6a7fd836530
Matthias Oßwald <mat.osswald@sap.com>
2025-05-16 14:18:50 +0200

    [FIX] manifestEnhancer: Only use valid files for supportedLocales (#1080)
    This fixes two problems that could have occurred:
    
    A properties file with an invalid locale was still taken into the list
    of supported locales, which then caused a runtime exception in the
    ResourceBundle as it validates the input.
    
    Another problem was that properties files could have a valid name
    according to BCP47, but the file won't be ever requested with that name.
    This is due to the fact that the ResourceBundle does use the legacy Java
    locale format (using underscores instead of dashes) for the request URL.
    
    In both cases, the properties file is now ignored and no entry for the
    supportedLocales is created.
    
    Only locales that are valid according to the legacy Java locale format
    are considered.
    However, there is one special case: sr_Latn is also requested by the UI5
    runtime, although it contains a BCP47 script, which is not valid
    according to the legacy Java locale format.
    
    ---------
    
    Co-authored-by: Merlin Beutlberger <m.beutlberger@sap.com>

--- test/expected/build/application.o/dest/Component-preload.js
+++ test/expected/build/application.o/dest/Component-preload.js
@@ -4,7 +4,7 @@ sap.ui.require.preload({
 	"application/o/i18n/i18n.properties":'welcome=Hello world',
 	"application/o/i18n/i18n_en.properties":'welcome=Hello EN world',
 	"application/o/i18n/i18n_en_US.properties":'welcome=Hello EN US world',
-	"application/o/i18n/i18n_en_US_sapprc.properties":'welcome=Hello EN US sapprc world',
-	"application/o/manifest.json":'{"_version":"1.22.0","sap.app":{"id":"application.o","type":"application","applicationVersion":{"version":"1.0.0"},"title":"{{title}}","i18n":{"bundleUrl":"i18n/i18n.properties","supportedLocales":["","en","en_US","en_US_sapprc"]}},"sap.ui5":{"models":{"i18n":{"type":"sap.ui.model.resource.ResourceModel","settings":{"bundleName":"application.o.i18n.i18n","supportedLocales":["","en","en_US","en_US_sapprc"]}},"i18n-ui5":{"type":"sap.ui.model.resource.ResourceModel","settings":{"bundleUrl":"ui5://application/o/i18n/i18n.properties","supportedLocales":["","en","en_US","en_US_sapprc"]}}}}}'
+	"application/o/i18n/i18n_en_US_saptrc.properties":'welcome=Hello EN US saptrc world',
+	"application/o/manifest.json":'{"_version":"1.22.0","sap.app":{"id":"application.o","type":"application","applicationVersion":{"version":"1.0.0"},"title":"{{title}}","i18n":{"bundleUrl":"i18n/i18n.properties","supportedLocales":["","en","en-US","en-US-x-saptrc"]}},"sap.ui5":{"models":{"i18n":{"type":"sap.ui.model.resource.ResourceModel","settings":{"bundleName":"application.o.i18n.i18n","supportedLocales":["","en","en-US","en-US-x-saptrc"]}},"i18n-ui5":{"type":"sap.ui.model.resource.ResourceModel","settings":{"bundleUrl":"ui5://application/o/i18n/i18n.properties","supportedLocales":["","en","en-US","en-US-x-saptrc"]}}}}}'
 });
 //# sourceMappingURL=Component-preload.js.map
