8c0a5dbe56119872b20dfa362c91f1958dd568c0
d3xter666 <yavor.ivanov@sap.com>
2025-10-16 15:15:07 +0300

    test: Update test fixtures

--- test/lib/tasks/bundlers/generateFlexChangesBundle.js
+++ test/lib/tasks/bundlers/generateFlexChangesBundle.js
@@ -456,7 +456,7 @@ test("sap.ui.fl dependency disables lazy loading if already present", async (t) 
 	t.false(sapUiFlDependency.lazy, "sap.ui.fl lazy loading should be disabled when bundle is created");
 });
 
-test("no manifest update when no changes exist", async (t) => {
+test("manifest updated with flexBundle false when no changes exist", async (t) => {
 	const manifest = {
 		"_version": "1.58.0",
 		"sap.app": {
@@ -492,8 +492,27 @@ test("no manifest update when no changes exist", async (t) => {
 		}
 	});
 
-	// Task should not execute when there are no changes
-	t.is(placeholderWorkspace.write.callCount, 0, "workspace.write should not be called when no changes exist");
+	// Manifest should always be updated, even when no changes exist
+	t.is(placeholderWorkspace.write.callCount, 1, "workspace.write should be called to update manifest");
+
+	// Find the manifest write call
+	let manifestCall;
+	for (let i = 0; i < placeholderWorkspace.write.callCount; i++) {
+		const call = placeholderWorkspace.write.getCall(i);
+		const path = call.args[0].getPath ? await call.args[0].getPath() : "unknown";
+		if (path && path.includes("manifest.json")) {
+			manifestCall = call;
+			break;
+		}
+	}
+
+	t.truthy(manifestCall, "Manifest should be written");
+	const manifestContent = JSON.parse(await manifestCall.args[0].getString());
+
+	t.truthy(manifestContent["sap.ui5"], "sap.ui5 section should exist");
+	t.false(manifestContent["sap.ui5"].flexBundle, "flexBundle should be set to false when no bundle is created");
+	t.is(manifestContent["sap.ui5"].dependencies.libs, undefined,
+		"sap.ui.fl dependency should not be added when no bundle is created");
 });
 
 test("flexBundle property overrides existing value when bundle is created", async (t) => {
