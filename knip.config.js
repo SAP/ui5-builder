3992559bf6829cd3daf396434843ba487bc484fb
Gianluca Beil <66752411+werwolf2303@users.noreply.github.com>
2026-02-12 15:37:48 +0100

    [INTERNAL] Replace depcheck with knip (#1195)
    Due to the deprecation of depcheck we migrated to knip, which is in
    active development and has more features. As a first step we only
    activate the checking of unused dependencies.
    
    The reason why we replaced the docdash jsdoc template path lookup in the
    jsdoc script is that knip fails to detect that a dependency (jsdoc) is
    used when a command inside of the package.json contains JavaScript code
    (via sub-shell).
    
    JIRA: CPOUI5FOUNDATION-1126

---
+++ knip.config.js
@@ -0,0 +1,33 @@
+const config = {
+	/**
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
+	 */
+	rules: {
+		files: "off",
+		duplicates: "off",
+		classMembers: "off",
+		unlisted: "off",
+		binaries: "off",
+		unresolved: "off",
+		catalog: "off",
+		exports: "off",
+		types: "off",
+		enumMembers: "off",
+	},
+
+	ignoreDependencies: [
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
+		"@istanbuljs/esm-loader-hook",
+
+		/**
+		 * Used as jsdoc template in package.json script, which is not detected
+		 */
+		"docdash"
+	],
+};
+
+export default config;
